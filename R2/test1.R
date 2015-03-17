004b28efead171e2fac2667b486f5cb440637c5e
Melkiades <dgarolini@gmail.com>
2015-03-17 13:41:07 +0100

    Using the advised downloader package to handle more stable https connection. It works well, thank you!

--- R2/test1.R
+++ R2/test1.R
@@ -4,7 +4,9 @@ source("R2/TCGAInternal.R")
 source("R2/TCGAUpdate.R")
 
 if(require(httr) == F) install.packages("httr")
+if(require(downloader) == F) install.packages("downloader")
 
+#TCGAUpdate()
 
 #after having updated the platform names matrix using:
 #TCGAUpdate()
@@ -15,3 +17,4 @@ if(require(httr) == F) install.packages("httr")
 x <- TCGAQuery(tumor = "acc", platform = "genome_wide_snp_6",centerType = "cgcc",level=3)
 
 TCGADownload(x,20) #earlyStop provided. Something about the stability of the connection and the ongoing situation must be done
+
