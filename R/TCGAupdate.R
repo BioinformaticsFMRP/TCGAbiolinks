c76a4c1ea97674149ddd45923d949b5de40fac4a
tiago <tiagochst@gmail.com>
2015-03-21 15:27:20 -0300

    Decided for data folder Removed siteTCGA and datatable from parameters, there is no reason to let the user change it.

--- R/TCGAupdate.R
+++ R/TCGAupdate.R
@@ -1,53 +1,55 @@
 #'@title TCGAUpdate
 #'
+#'@description replace the dataFolders.rda matrix with the
+#'             new matrix from "The Cancer Genome Atlas" ftp
 #'@author Davide
-#'
-#' @export
-TCGAUpdate <-function(siteTCGA="https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/"){
-  PlatformMat <- NULL
+#'@seealso TCGAQuery
+#'@export
+TCGAUpdate <-function(){
+  siteTCGA <- "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/"
+  dataFolders <- NULL
 
   x <- DownloadHTML(siteTCGA)
   x <- GrepSite(x, "href")
 
   Tumor <- sub("/", "", x)
 
-  siteTumor <- paste0(siteTCGA, Tumor, "/")
-
+  siteTumor <- paste(siteTCGA, Tumor, "/", sep = "")
   for(tt in 1:length(siteTumor)){
     x <- DownloadHTML(siteTumor[tt])
     x <- GrepSite(x, "href")
     CenterType <- sub("/", "", x)
 
-    siteCenterType <- paste0(siteTumor[tt], CenterType, "/")
+    siteCenterType <- paste(siteTumor[tt], CenterType, "/", sep = "")
     for(ct in 1:length(siteCenterType)){
       x <- DownloadHTML(siteCenterType[ct])
       x <- GrepSite(x, "href")
       Center <- sub("/", "", x)
 
-      siteCenter <- paste0(siteCenterType[ct], Center, "/")
+      siteCenter <- paste(siteCenterType[ct], Center, "/", sep = "")
       for(cc in 1:length(siteCenter)){
         x <- DownloadHTML(siteCenter[cc])
         x <- GrepSite(x, "href")
         Platform <- sub("/", "", x)
 
-        sitePlatform <- paste0(siteCenter[cc],Platform, "/")
+        sitePlatform <- paste(siteCenter[cc],Platform, "/", sep = "")
         for(p in 1:length(sitePlatform)){
           x <- DownloadHTML(sitePlatform[p])
           x <- GrepSite(x, "href")
           kind <- sub("/", "", x)
 
-          siteKind <- paste0(sitePlatform[p],kind, "/")
+          siteKind <- paste(sitePlatform[p],kind, "/", sep = "")
           for(k in 1:length(siteKind)){
             x <- DownloadHTML(siteKind[k])
             x <- GrepSite(x, "href")
             folder <- sub("/", "", x)
 
-            siteFolder <- paste0(siteKind[k],folder, "/")
+            siteFolder <- paste(siteKind[k],folder, "/", sep = "")
 
             if(length(Platform)==0) Platform = " "
             if(length(kind)==0) kind = " "
             if(length(folder)==0) folder = " "
-            PlatformMat <- rbind(PlatformMat,
+            dataFolders <- rbind(dataFolders,
                                  cbind(
                                    Tumor = rep(Tumor[tt], length(folder)),
                                    CenterType = rep(CenterType[ct], length(folder)),
@@ -55,15 +57,16 @@ TCGAUpdate <-function(siteTCGA="https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth
                                    Platform = rep(Platform[p],length(folder)),
                                    Kind = rep(kind[k],length(folder)),
                                    Folder = folder,
-                                   Manifest = paste0(siteFolder,"MANIFEST.txt")))
+                                   Manifest = paste(siteFolder,"MANIFEST.txt",sep="")))
           }
         }
         print(paste(Tumor[tt], CenterType[ct], Center[cc], ":", paste(Platform, collapse = " - "), "done!", sep = " "))
       }
     }
   }
-  save(PlatformMat,
-       file = paste(system.file("extdata", package="TCGAbiolinks"),
-                                 "PlatformMat.rda",sep="/")
-       )
+  dir.create(tableLocation, showWarnings = F, recursive = T, mode = "0777")
+  save(dataFolders,
+       file = paste(system.file("data", package="TCGAbiolinks"),
+                    "dataFolders.rda",sep="/")
+  )
 }
