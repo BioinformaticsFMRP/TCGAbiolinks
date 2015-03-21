018cc7afebaeb4c8179b5dc85fab025036d9c535
tiago <tiagochst@gmail.com>
2015-03-20 22:58:57 -0300

    Updating TCGAQuery with the last version, took it from R2 folder

--- R/TCGAQuery.R
+++ R/TCGAQuery.R
@@ -1,60 +1,89 @@
-#' @title TCGA Query
+#' @title TCGA query
 #'
 #' @description  TCGA Query
 #'
 #' @param Tumor -  tumor code
-#' @param siteTCGA - Plataform
+#' @param centerType
+#' @param center
+#' @param platform
+#' @param level
+#' @param version
+#' @param i - interactive
+#' @param file - link reference data matrix
 #' @export
 
+TCGAQuery <- function(tumor = "all",
+                      centerType = "all",
+                      center = "all",
+                      platform = "all",
+                      level = "all",
+                      version = "all",
+                      i = F,
+                      file = system.file("extdata/PlatformMat.rda",
+                                         package="TCGAbiolinks")
+                      ){
+  load(file = file) #please add a way to access the package data storing
+  if(!i){
+    ifelse(tumor != "all",x<-subset(PlatformMat, PlatformMat[,"Tumor"] == tolower(tumor)),x<-PlatformMat)
 
-TCGAQuery <- function(Tumor,sdrfFolder,PlatformAndAssociatedData){
-  message(Tumor)
-  tmplf <- PlatformAndAssociatedData[PlatformAndAssociatedData$Tumor %in% tolower(Tumor),]
-  TumorDataList <- vector("list", length(unique(tmplf$Platform)))
-  names(TumorDataList) <- unique(tmplf$Platform)
-  FolderWd <- getwd()
-  setwd(sdrfFolder)
-  
-  for( i in 1: nrow(tmplf)){
-    startK <- 1
-    stopK <- 16
-    if(tmplf[i,"FileName"] == "") next
-    currentTFile <- gsub("/","_",tmplf[i,"FileName"])
-    
-    
-    currentTFile<-paste(sdrfFolder,currentTFile,sep="")
-    tmp2 <- read.delim(currentTFile,stringsAsFactors = F,skip =  tmplf[i,"KeySkip"], header =T )
-    
-    if( length(intersect(colnames(tmp2), tmplf[i,"nameCol"]))==1){ tmp2 <- tmp2[, tmplf[i,"nameCol"]  ] }
-    if( length(intersect(colnames(tmp2), "Sample.description"))==1){ tmp2 <- tmp2[, "Sample.description"] }
-    if( length(intersect(colnames(tmp2), "Biospecimen.Barcode"))==1){ tmp2 <- tmp2[, "Biospecimen.Barcode"] }
-    if( length(intersect(colnames(tmp2), "Comment..TCGA.Barcode."))==1){ tmp2 <- tmp2[, "Comment..TCGA.Barcode."] }
-    tmp2 <- as.matrix(tmp2)
-    if( ncol(tmp2)!=1){
-      if( length( grep("nationwidechildrens", tmplf[i,"FileName"]))==1 || length( grep("biospecimen", tmplf[i,"FileName"]))==1 || length( grep("bio.Level", tmplf[i,"FileName"]))==1   ){
-        tmp2 <- read.table(currentTFile, quote="\"",stringsAsFactors = F )[,2]
-        tmp2 <- tmp2[grep("TCGA",tmp2)]
-        if( length( grep("nationwidechildrens",tmp2 ))==1 || length( grep("biospecimen", tmp2))==1 ){
-          if ( length(tmp2)!=0){ tmp2 <- as.matrix(unlist(strsplit(tmp2, ".",fixed = T))) }
-        }
+    if(centerType != "all" && is.null(nrow(x))) x<-subset(x, x["CenterType"] == tolower(centerType))
+    if(centerType != "all" && !is.null(nrow(x))) x<-subset(x, x[,"CenterType"] == tolower(centerType))
+
+
+    if(platform != "all" && is.null(nrow(x))) x<-subset(x, x["Platform"] == tolower(platform))
+    if(platform != "all" && !is.null(nrow(x))) x<-subset(x, x[,"Platform"] == tolower(platform))
+
+
+    if(level != "all"){
+      if(is.null(nrow(x))){
+        l <- grep(paste("Level_",as.character(level),sep=""),x["Folder"])
+        if(length(l)==0) x<-NULL
+      }else{
+        l <- grep(paste("Level_",as.character(level),sep=""),x[,"Folder"])
+        x<-x[l,]
       }
     }
-    
-    tmp2 <- tmp2[grep("TCGA",tmp2)]
-    SampleTmp <- unique(substr(tmp2, startK, stopK))
-    SampleTmp <- SampleTmp[grep("TCGA",SampleTmp)]
-    msgOUT <-  paste(i," ", Tumor," ", tmplf[i,"Type"], " ", tmplf[i,"Species"], " ",   tmplf[i,"Center"], " ", tmplf[i,"Platform"] , " " ,  " .n samples ", length(SampleTmp), sep="")
-    print(msgOUT)
-    
-    if ( length(SampleTmp) < 2) { message("Maybe.....ERROR....Check IT")}
-    
-    idx<- which(names(TumorDataList) == tmplf[i,"Platform"])
-    TumorDataList[[idx]] <- SampleTmp
-    
+    #     if(version != "all"){ #something smarter could be done (maybe "lastUp" as keyword)
+    #       if(is.null(nrow(x)))l <- grep(paste("Level_",as.character(version),sep=""),x["Folder"])
+    #       if(!is.null(nrow(x)))l <- grep(paste("Level_",as.character(version),sep=""),x[,"Folder"])
+    #       if(length(l)>1) x<-x[l,]
+    #       if(length(l)==0)x<-NULL
+    #     }
+
+    if(is.null(x)){
+      return("Nothing found. Check the proper spelling in the documentation.")
+    }else if(is.null(nrow(x))) {
+      print("Found: 1 folder. Start downloading filenames:")
+    }else{
+      print(paste("Found:", length(x[,1]), "folders. Start downloading filenames:",sep=" "))
+    }
+
+    ret = NULL
+    dataDir<-createDir("data/query")
+    if(is.null(nrow(x))){
+      download(x["Manifest"],
+               destfile = paste(dataDir,"/filenames.txt",sep=""),
+               mode="a",
+               quiet = 0)
+      ret <- paste(unlist(strsplit(x["Manifest"], split='MANIFEST.txt', fixed=TRUE)),
+                   as.character(read.table(file = paste(dataDir,"/filenames.txt",sep=""))[2]$V2),sep="")
+      print("Donwloaded.")
+    }else{
+      for(j in 1:length(x[,"Tumor"])){
+        download(x[,"Manifest"][j],
+                 destfile = paste(dataDir,"/filenames.txt",sep=""),
+                 mode="w",
+                 quiet = 0) #character. The mode with which to write the file.
+        #Useful values are "w", "wb" (binary), "a" (append) and "ab".
+        #Only used for the "internal" method.
+        #APPEND IS NOT WORKING
+
+        print(paste("Downloaded:",j,"out of",length(x[,"Tumor"]),sep=" "))
+        ret<-c(ret,paste(unlist(strsplit(x[,"Manifest"][j], split='MANIFEST.txt', fixed=TRUE)),
+                         as.character(read.table(file = paste(dataDir,"/filenames.txt",sep=""))[2]$V2),sep=""))
+      }
+    }
   }
-  #Tumor_completeLIST[[k]]<-TumorDataList
-  
-  setwd(FolderWd)
-  return(TumorDataList)
-  
+  print(paste("We found",length(ret),"files",sep=" "))
+  return(ret)
 }
