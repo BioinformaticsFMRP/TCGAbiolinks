6ab3c4b0ab98ed64ba350d40c8456f6af4e67f67
tiago <tiagochst@gmail.com>
2015-03-20 22:33:58 -0300

    Adding new TCGA Update into main code. Mergin old internal functions and new into one file. TODO: remove duplicated internal functions.

--- R/internal.R
+++
@@ -1,8 +0,0 @@
-# @title .onAttach
-# @description  Load required data into gloval enviroment
-# @keywords internal
-.onAttach <- function (libname, pkgname){
-  load(file = system.file("extdata/PlatformAndAssociatedData_WebSites3.RData", 
-                          package="TCGAbiolinks"), 
-       .GlobalEnv) 
-}
