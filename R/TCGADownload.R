5c4b116909ab71b95409ad6f97373cfa562d26c1
Tiago Chedraoui Silva <tiagochst@gmail.com>
2015-05-04 17:19:02 -0300

    Starting demo2

--- R/TCGADownload.R
+++ R/TCGADownload.R
@@ -13,10 +13,25 @@ TCGADownload <- function(data=NULL,path=".")
 {
   dir.create(path,showWarnings = F)
   root <- "https://tcga-data.nci.nih.gov"
-  for(i in 1:nrow(data)){
-    file <- paste0(path,"/",basename(data[i,"deployLocation"]))
-    message(paste0("Downloading:", basename(data[i,"deployLocation"])))
-    downloader::download(paste0(root,data[i,"deployLocation"]),file)
-    untar(file, exdir = path)
+  if(!("file" %in% colnames(data))){
+    message("Downloading folders")
+    for(i in 1:nrow(data)){
+      file <- paste0(path,"/",basename(data[i,"deployLocation"]))
+      message(paste0("Downloading:", basename(data[i,"deployLocation"])))
+      if(!file.exists(file)){
+        downloader::download(paste0(root,data[i,"deployLocation"]),file)
+        untar(file, exdir = path)
+      }
+    }
+  }
+  else{
+    message("Downloading files")
+    for(i in 1:nrow(data)){
+      file <- paste0(path,"/",basename(data[i,"file"]))
+      message(paste0("Downloading:", basename(data[i,"file"])))
+      if(!file.exists(file)){
+        downloader::download(paste0(root,gsub(".tar.gz","",data[i,"deployLocation"]),"/",data[i,"file"]),file)
+      }
+    }
   }
 }
