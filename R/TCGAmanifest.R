31c5e44b897a29f6c9d9dca61551902ba90e3c12
torongs82 <antonio.colaprico@gmail.com>
2015-05-13 17:12:59 +0200

    Version to submit

---
+++ R/TCGAmanifest.R
@@ -0,0 +1,33 @@
+TCGAmanifest <-
+function(Tumor){
+   FolderWd <- getwd()
+  siteTCGA <- "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/"   
+  #.createDirectory("TCGAsdrf")
+
+  
+  xs <- paste(getwd(),Tumor, sep="/")            
+  
+  dir.create(xs)
+
+  setwd(xs)
+  xd <- paste(getwd(),"/TCGAsdrf", sep="") 
+  dir.create(xd)
+  setwd(xd)
+  
+  
+  tmplf <- PlatformAndAssociatedData[PlatformAndAssociatedData$Tumor %in% tolower(Tumor),]
+  token <- 1
+  for ( w in token: nrow( tmplf)){
+    tmp <- tmplf[w,]
+    if(tmp["FileName"] == "") next
+    key1a<- paste(unique(tmp$CenterType), unique(tmp$Center), unique(tmp$Platform), sep="/")
+    Description <- paste(siteTCGA, tolower(tmp$Tumor), "/",key1a, sep="")
+    DescriptionSite <- paste(Description, tmp$Folder, tmplf[w,"FileName"], sep="/")
+    x <- .DownloadURL(DescriptionSite)
+    #x <- .DownloadData_fromURL(DescriptionSite)
+    writeLines(x, gsub("/","_",tmplf[w,"FileName"]))
+    
+    print(paste(w, " of ", nrow(tmplf ), " ", tmplf[w,"FileName"]),sep="")
+  }
+  #setwd(xs)
+}
