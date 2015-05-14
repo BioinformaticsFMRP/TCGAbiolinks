de5839b2448131a9488af782088186b03a6c33e5
tiago <tiagochst@gmail.com>
2015-05-14 11:30:59 -0300

    Passing cran

--- inst/examples/tcgaSearch.R
+++ inst/examples/tcgaSearch.R
@@ -1,20 +1,20 @@
-query <- tcgaSearch(tumor = "gbm",
+query <- tcgaQuery(tumor = "gbm",
                     added.since = "01/01/2013",
                     added.up.to = "06/01/2013")
 
-query <- tcgaSearch(tumor = c("gbm","lgg"),
+query <- tcgaQuery(tumor = c("gbm","lgg"),
                     platform = c("HumanMethylation450","HumanMethylation27"))
 
-query <- tcgaSearch(tumor = "gbm", platform = "bio")
+query <- tcgaQuery(tumor = "gbm", platform = "bio")
 
-query <- tcgaSearch(tumor = "gbm", platform = "HumanMethylation450",
+query <- tcgaQuery(tumor = "gbm", platform = "HumanMethylation450",
                     level = "3")
 
-query <- tcgaSearch(listSample = "TCGA-61-1743-01A-01D-*")
+query <- tcgaQuery(samples = "TCGA-61-1743-01A-01D-*")
 
-query <- tcgaSearch(listSample = "TCGA-61-1743-01A-01D-0649-04")
+query <- tcgaQuery(samples = "TCGA-61-1743-01A-01D-0649-04")
 
-query <- tcgaSearch(listSample = "TCGA-61-1743-01A-01D-0649-04", level = "3")
+query <- tcgaQuery(samples = "TCGA-61-1743-01A-01D-0649-04", level = 3)
 
-query <- tcgaSearch(listSample = "TCGA-61-1743-01A-01D-0649-04",
+query <- tcgaQuery(samples = "TCGA-61-1743-01A-01D-0649-04",
                     tumor = "OV", platform = "CGH-1x1M_G4447A")
