2e55f7d08741aae092f5524d3f9b9827d7bfb85a
torongs82 <antonio.colaprico@gmail.com>
2015-05-17 18:28:44 +0200

    Adding vignette for gene expression Downstream analysis and clinical functions.

---
+++ R/clinical.R
@@ -0,0 +1,231 @@
+#' @title SampleTypes_test
+#' @description
+#'   SampleTypes_test
+#' @param barcode barcode
+#' @param typesample typesample
+#' @import
+#' @export
+#' @return SampleTypes_test
+SampleTypes_test <- function(barcode,typesample){
+  table.code <- c('01','02','03','04','05','06','07','08','09','10','11','12','13','14','20','40','50','60','61')
+  names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC","TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB","CELL","XP","XCL")
+
+  if(is.element(typesample,names(table.code))){
+    barcode <- barcode[grep(table.code[typesample], substr(barcode, 14, 15))]
+  }else{
+    return("Error message: sample type does not exist")
+  }
+  return(barcode)
+}
+
+#' @title MultiSampleTypes_test
+#' @description
+#'   MultiSampleTypes_test
+#' @param barcode barcode
+#' @param typesample typesample
+#' @import
+#' @export
+#' @return MultiSampleTypes_test
+MultiSampleTypes_test <- function(barcode,typesample){
+  # Tumor AND Solid Tissue Normal NOT FROM THE SAME PATIENTS
+  table.code <- c('01','02','03','04','05','06','07','08','09','10','11','12','13','14','20','40','50','60','61')
+  names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC","TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB","CELL","XP","XCL")
+
+  if(sum(is.element(typesample,names(table.code))) == length(typesample)) {
+
+    string <- substr(barcode, 14, 15)
+    barcode.all <- NULL
+    for(sample.i in typesample){
+      barcode.all <- union(barcode.all, barcode[grep(table.code[sample.i], string)])
+    }
+    return(barcode.all)
+  }else{
+    return("Error message: one or more sample types do not exist")
+  }
+}
+
+#' @title MatchedCoupledSampleTypes_test
+#' @description
+#'   MatchedCoupledSampleTypes_test
+#' @param barcode barcode
+#' @param typesample typesample
+#' @import
+#' @export
+#' @return MultiSampleTypes_test
+MatchedCoupledSampleTypes_test <- function(barcode,typesample){
+  # Tumor AND Solid Tissue Normal FROM THE SAME PATIENTS
+  table.code <- c('01','02','03','04','05','06','07','08','09','10','11','12','13','14','20','40','50','60','61')
+  names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC","TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB","CELL","XP","XCL")
+  if(length(typesample)!=2){
+    return("Error message: exactly two types need to be provided")
+  }
+
+  if(sum(is.element(typesample,names(table.code))) == length(typesample)) {
+
+    string <- substr(barcode, 14, 15)
+    barcode.1 <- barcode[grep(table.code[typesample[1]], string)]
+    barcode.2 <- barcode[grep(table.code[typesample[2]], string)]
+
+
+    barcode.common <- intersect(substr(barcode.1,1,13), substr(barcode.2,1,13))
+    if(length(barcode.common) > 0){
+      return(union(barcode.1[grep(barcode.common,barcode.1)],barcode.2[grep(barcode.common,barcode.2)]))
+    }else{
+      return("Error message: there exist no matched samples")
+    }
+  } else{
+    return("Error message: one or more sample types do not exist")
+  }
+}
+
+#' @title stage_BRCA_test
+#' @description
+#'   stage_BRCA_test
+#' @param barcode barcode
+#' @param stage stage
+#' @param clinical_patient_data clinical_patient_data
+#' @import
+#' @export
+#' @return stage_BRCA_test
+stage_BRCA_test <- function(barcode, stage, clinical_patient_data){
+  table.stages <- c("Stage I$|Stage IA$|Stage IB$", "Stage I$", "Stage IA$", "Stage IB$", "Stage II$|Stage IIA$|Stage IIB$", "Stage II$",
+                    "Stage IIA$", "Stage IIB$", "Stage III$|Stage IIIA$|Stage IIIB$|Stage IIIC$", "Stage III$", "Stage IIIA$", "Stage IIIB$", "Stage IIIC$", "Stage IV$")
+  names(table.stages) <- c("stage_IX", "stage_I", "stage_IA", "stage_IB", "stage_IIX", "stage_IIA", "stage_IIB", "stage_IIIX", "stage_IIIA",
+                           "stage_IIIB", "stage_IIIC", "stage_IV")
+
+  if(is.element(stage, names(table.stages))) {
+    clinical_patient_data<-as.data.frame(clinical_patient_data)
+    stage.i <- clinical_patient_data[grep(table.stages[stage], clinical_patient_data$ajcc_pathologic_tumor_stage), ]
+    stage.i <- stage.i[,"bcr_patient_barcode"]
+    samples <- substr(barcode, 1, 12)
+    barcode <- intersect(samples,stage.i)
+  }else{
+    return("Error message: stage does not exist")
+  }
+  return(barcode)
+}
+
+#' @title gender_BRCA_test
+#' @description
+#'   gender_BRCA_test
+#' @param barcode barcode
+#' @param gender gender
+#' @param clinical_patient_data clinical_patient_data
+#' @import
+#' @export
+#' @return stage_BRCA_test
+gender_BRCA_test <- function(barcode, gender, clinical_patient_data){
+
+  if(is.element(gender,c("MALE", "FEMALE"))){
+    clinical_patient_data<-as.data.frame(clinical_patient_data)
+    s.gender <- as.data.frame(clinical_patient_data)[grep(paste0("^", gender,"$"), clinical_patient_data$gender), ][,"bcr_patient_barcode"]
+    samples<-substr(barcode, 1, 12)
+    #find common patients between FEMALE e barcode data
+    barcode<-intersect(samples,s.gender)
+  }else{
+    return("Error message gender doesn't exist")
+  }
+
+  return(barcode)
+}
+
+#' @title ER_status_BRCA_test
+#' @description
+#'   ER_status_BRCA_test
+#' @param barcode barcode
+#' @param ER ER
+#' @param clinical_patient_data clinical_patient_data
+#' @import
+#' @export
+#' @return ER_status_BRCA_test
+ER_status_BRCA_test <- function(barcode,ER, clinical_patient_data){
+  ## ER should be "Positive" or "Negative"
+  # consider only barcode and ER status
+  if(is.element(ER, c("Positive", "Negative"))){
+    status <- as.data.frame(clinical_patient_data)[grep(paste0("^",ER,"$"), clinical_patient_data$er_status_by_ihc), ][,"bcr_patient_barcode"]
+    samples<-substr(barcode, 1, 12)
+    #find common patients between ER status and barcode data
+    barcode<-intersect(samples,status)
+    return(barcode)
+  }else{
+    return("Error message: ER status does not exist")
+  }
+}
+
+#' @title PR_status_BRCA_test
+#' @description
+#'   PR_status_BRCA_test
+#' @param barcode barcode
+#' @param PR PR
+#' @param clinical_patient_data clinical_patient_data
+#' @import
+#' @export
+#' @return PR_status_BRCA_test
+PR_status_BRCA_test <- function(barcode,PR, clinical_patient_data){
+  ## PR should be "Positive" or "Negative"
+
+  if(is.element(PR, c("Positive", "Negative"))){
+    #for breast cancer
+    status <- as.data.frame(clinical_patient_data)[grep(paste0("^", PR, "$"), clinical_patient_data$pr_status_by_ihc), ][,"bcr_patient_barcode"]
+    samples<-substr(barcode, 1, 12)
+    #find common patients between PR status and barcode data
+    barcode<-intersect(samples,status)
+  }else{
+    return("Error message: PR status does not exist")
+  }
+
+  return(barcode)
+
+}
+
+#' @title HER_status_BRCA_test
+#' @description
+#'   HER_status_BRCA_test
+#' @param barcode barcode
+#' @param HER HER
+#' @param clinical_patient_data clinical_patient_data
+#' @import
+#' @export
+#' @return HER_status_BRCA_test
+HER_status_BRCA_test <- function(barcode, HER, clinical_patient_data){
+  if(is.element(HER, c("Positive", "Negative"))){
+    clinical_patient_data<-as.data.frame(clinical_patient_data)
+    #for breast cancer HER+
+    status <- as.data.frame(clinical_patient_data)[grep(paste0("^",HER,"$"), clinical_patient_data$her2_status_by_ihc), ][,"bcr_patient_barcode"]
+    samples<-substr(barcode, 1, 12)
+    #find common patients between HER+ e barcode data
+    barcode<-intersect(samples,status)
+    return(barcode)
+  }else{
+    return("Error message: HER status does not exist")
+  }
+
+
+}
+
+#' @title clinical_data_site_cancer_test
+#' @description
+#'   clinical_data_site_cancer_test
+#' @param cancer cancer
+#' @import
+#' @export
+#' @return clinical_data_site_cancer_test
+clinical_data_site_cancer_test<-function(cancer){
+  return(paste0("https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/", cancer,"/bcr/biotab/clin/"))
+}
+
+#' @title clinic
+#' @description
+#'   clinic
+#' @param cancer cancer
+#' @param clinical_data_type clinical_data_type
+#' @importFrom RCurl getURL
+#' @export
+#' @return clinic
+clinic<-function(cancer,clinical_data_type){
+
+  URL<- paste0(clinical_data_site_cancer_test(cancer), "nationwidechildrens.org_", clinical_data_type, "_", cancer, ".txt")
+  writeLines(getURL(URL,ssl.verifypeer = FALSE), file(paste0(clinical_data_type,".txt")))
+
+  return(file(paste0(clinical_data_type,".txt")))
+}
