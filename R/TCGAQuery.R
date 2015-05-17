a25f482468d2fa08566a461add06c709c9910da8
torongs82 <antonio.colaprico@gmail.com>
2015-05-17 13:23:27 +0200

    checking documentation of TCGAQuery

--- R/TCGAQuery.R
+++ R/TCGAQuery.R
@@ -1,3 +1,180 @@
+# ------------------------ Encode search
+#' @title TCGAQuery
+#' @description
+#'    Searches in the tcga database
+#' @param tumor Disease Examples:
+#' \tabular{lllll}{
+#'OV   \tab BRCA \tab CESC \tab ESCA \tab PCPG\cr
+#'LUSC \tab LGG  \tab SKCM \tab KICH \tab CHOL\cr
+#'GBM  \tab UCEC \tab PRAD \tab PAAD \tab THYM\cr
+#'KIRC \tab THCA \tab SARC \tab LAML \tab TGCT\cr
+#'COAD \tab KIRP \tab HNSC \tab ACC  \tab UVM \cr
+#'READ \tab BLCA \tab DLBC \tab UCS  \tab FPPP\cr
+#'LUAD \tab LIHC \tab STAD \tab MESO \tab CNTL
+#'}
+#' @param platform Example:
+#' \tabular{ll}{
+#'CGH- 1x1M_G4447A                   \tab IlluminaGA_RNASeqV2   \cr
+#'AgilentG4502A_07                  \tab IlluminaGA_mRNA_DGE    \cr
+#'Human1MDuo                        \tab HumanMethylation450    \cr
+#'HG-CGH-415K_G4124A                \tab IlluminaGA_miRNASeq    \cr
+#'HumanHap550                       \tab IlluminaHiSeq_miRNASeq \cr
+#'ABI                               \tab H-miRNA_8x15K  \cr
+#'HG-CGH-244A                       \tab SOLiD_DNASeq                       \cr
+#'IlluminaDNAMethylation_OMA003_CPI \tab IlluminaGA_DNASeq_automated        \cr
+#'IlluminaDNAMethylation_OMA002_CPI \tab HG-U133_Plus_2                 \cr
+#'HuEx- 1_0-st-v2 \tab Mixed_DNASeq                  \cr
+#'H-miRNA_8x15Kv2 \tab IlluminaGA_DNASeq_curated      \cr
+#'MDA_RPPA_Core   \tab IlluminaHiSeq_TotalRNASeqV2    \cr
+#'HT_HG-U133A     \tab IlluminaHiSeq_DNASeq_automated \cr
+#'diagnostic_images                 \tab microsat_i                     \cr
+#'IlluminaHiSeq_RNASeq              \tab SOLiD_DNASeq_curated           \cr
+#'IlluminaHiSeq_DNASeqC             \tab Mixed_DNASeq_curated           \cr
+#'IlluminaGA_RNASeq                 \tab IlluminaGA_DNASeq_Cont_automated   \cr
+#'IlluminaGA_DNASeq                 \tab IlluminaHiSeq_WGBS             \cr
+#'pathology_reports                 \tab IlluminaHiSeq_DNASeq_Cont_automated\cr
+#'Genome_Wide_SNP_6                 \tab bio                            \cr
+#'tissue_images                     \tab Mixed_DNASeq_automated         \cr
+#'HumanMethylation27                \tab Mixed_DNASeq_Cont_curated      \cr
+#'IlluminaHiSeq_RNASeqV2            \tab Mixed_DNASeq_Cont
+#'}
+#' @param level '1' '2' '3'
+#' @param added.since 04- 14-2010
+#' @param added.up.to 04- 14-2010
+#' @param center center name
+#' @param samples List of samples. Ex:c('TCGA-04-06-*','TCGA-04-08-*')
+#' @example inst/examples/tcgaSearch.R
+#' @export
+#' @importFrom downloader download
+#' @importFrom knitr kable
+#' @return A dataframe with the results of the query
+#'        (lastest version of the files)
+TCGAQuery <- function(tumor = NULL, platform = NULL, added.since = NULL,
+                      added.up.to = NULL, samples = NULL, center = NULL,
+                      level = NULL) {
+
+  disease.table   <- get("disease.table")
+  platform.table  <- get("platform.table")
+  center.table  <- get("center.table")
+  db <-  get("tcga.db")
+
+  if (!is.null(tumor)) {
+    sapply(tumor, function(x){
+      if (!(is.element(tolower(x),
+                       tolower(disease.table$abbreviation)))) {
+        suppressWarnings(
+          df <- as.data.frame(matrix(sort(unique(disease.table$abbreviation)),
+                                     ncol = 8))
+        )
+        print(kable(df, col.names = NULL, format = "pandoc",
+                    caption = "TCGA tumors"))
+        cat("=======================================================\n")
+        cat("ERROR: Disease not found. Select from the table above.\n")
+        cat("=======================================================\n")
+      }
+      return(NULL)
+    })
+  }
+  if (!is.null(platform)) {
+    sapply(platform, function(x){
+      if (!(is.element(tolower(x), tolower(platform.table$name)))) {
+        suppressWarnings(
+          df <- as.data.frame(matrix(sort(unique(platform.table$name)),
+                                     ncol = 3))
+        )
+        print(kable(df, col.names = NULL, format = "pandoc",
+                    caption = "TCGA Platforms"))
+        cat("=======================================================\n")
+        cat("ERROR: Platform not found. Select from the table above.\n")
+        cat("=======================================================\n")
+        return(NULL)
+      }
+    })}
+
+  if (!is.null(center)) {
+    if (!(is.element(tolower(center), tolower(center.table$name)))) {
+      suppressWarnings(
+        df <- as.data.frame(matrix(sort(unique(center.table$name)),
+                                   ncol = 3))
+      )
+      print(kable(df, col.names = NULL, format = "pandoc",
+                  caption = "TCGA Centers"))
+      cat("=======================================================\n")
+      cat("ERROR: Center not found. Select from the table above.\n")
+      cat("=======================================================\n")
+      return(NULL)
+    }
+  }
+  if (!is.null(level)) {
+    if (!(is.element(level, c("1", "2", "3","mage-tab")))) {
+      message("Level not found. Chosse between:'1', '2','3','mage-tab'")
+      stop("Invalid platform")
+    }
+  }
+
+  if (!is.null(added.since)) {
+    d <- try(as.Date(added.since, format = "%Y/%m/%d"))
+    if (class(d) == "try-error" || is.na(d)) {
+      print("Date format should be YYYY-mm-dd")
+    }
+  }
+  if (!is.null(added.up.to)) {
+    d <- try(as.Date(added.up.to, format = "%Y/%m/%d"))
+    if (class(d) == "try-error" || is.na(d)) {
+      print("Date format should be YYYY-mm-dd")
+    }
+  }
+
+
+  if(!is.null(tumor)){
+    id <- sapply(tumor, function(x){
+      grepl(x, db$Disease, ignore.case = TRUE)
+    })
+    id <- apply(id, 1,any)
+    db <-  db[id,]
+  }
+  if(!is.null(platform)){
+    id <- sapply(platform, function(x){
+      grepl(x, db$Platform, ignore.case = TRUE)
+    })
+    id <- apply(id, 1,any)
+    db <-  db[id,]
+  }
+  if(!is.null(center)){
+    id <- sapply(center, function(x){
+      grepl(x, db$Center, ignore.case = TRUE)
+    })
+    id <- apply(id, 1,any)
+    db <-  db[id,]
+  }
+  if(!is.null(level)){
+    id <- grep(paste0("Level_", level), db$name)
+    if(length(id) > 0){
+      db <-  db[id,]
+    }
+  }
+
+  if (!is.null(added.since)) {
+    db <- subset(db, as.Date(db$addedDate) > as.Date(added.since,
+                                                     "%m/%d/%Y"))
+  }
+  if (!is.null(added.up.to)) {
+    db <- subset(db, as.Date(db$addedDate) < as.Date(added.up.to,
+                                                     "%m/%d/%Y"))
+  }
+
+  # to be improved
+  idx <- c()
+  if(!is.null(samples)){
+    for(i in seq_along(samples)){
+      aux <- grep(samples[i],db$barcode)
+      idx <- union(idx, aux)
+    }
+    db <- db[idx,]
+  }
+  return(db)
+}
+
 getBarcode <- function(table){
   tcga.db <- get("tcga.db")
   root <- "https://tcga-data.nci.nih.gov"
@@ -216,182 +393,5 @@ getBarcode <- function(table){
   unlink(folders, recursive = TRUE)
 
   return(table)
-}
-
-# ------------------------ Encode search
-#' @title TCGAQuery
-#' @description
-#'    Searches in the tcga database
-#' @param tumor Disease Examples:
-#' \tabular{lllll}{
-#'OV   \tab BRCA \tab CESC \tab ESCA \tab PCPG\cr
-#'LUSC \tab LGG  \tab SKCM \tab KICH \tab CHOL\cr
-#'GBM  \tab UCEC \tab PRAD \tab PAAD \tab THYM\cr
-#'KIRC \tab THCA \tab SARC \tab LAML \tab TGCT\cr
-#'COAD \tab KIRP \tab HNSC \tab ACC  \tab UVM \cr
-#'READ \tab BLCA \tab DLBC \tab UCS  \tab FPPP\cr
-#'LUAD \tab LIHC \tab STAD \tab MESO \tab CNTL
-#'}
-#' @param platform Example:
-#' \tabular{ll}{
-#'CGH- 1x1M_G4447A                   \tab IlluminaGA_RNASeqV2   \cr
-#'AgilentG4502A_07                  \tab IlluminaGA_mRNA_DGE    \cr
-#'Human1MDuo                        \tab HumanMethylation450    \cr
-#'HG-CGH-415K_G4124A                \tab IlluminaGA_miRNASeq    \cr
-#'HumanHap550                       \tab IlluminaHiSeq_miRNASeq \cr
-#'ABI                               \tab H-miRNA_8x15K  \cr
-#'HG-CGH-244A                       \tab SOLiD_DNASeq                       \cr
-#'IlluminaDNAMethylation_OMA003_CPI \tab IlluminaGA_DNASeq_automated        \cr
-#'IlluminaDNAMethylation_OMA002_CPI \tab HG-U133_Plus_2                 \cr
-#'HuEx- 1_0-st-v2 \tab Mixed_DNASeq                  \cr
-#'H-miRNA_8x15Kv2 \tab IlluminaGA_DNASeq_curated      \cr
-#'MDA_RPPA_Core   \tab IlluminaHiSeq_TotalRNASeqV2    \cr
-#'HT_HG-U133A     \tab IlluminaHiSeq_DNASeq_automated \cr
-#'diagnostic_images                 \tab microsat_i                     \cr
-#'IlluminaHiSeq_RNASeq              \tab SOLiD_DNASeq_curated           \cr
-#'IlluminaHiSeq_DNASeqC             \tab Mixed_DNASeq_curated           \cr
-#'IlluminaGA_RNASeq                 \tab IlluminaGA_DNASeq_Cont_automated   \cr
-#'IlluminaGA_DNASeq                 \tab IlluminaHiSeq_WGBS             \cr
-#'pathology_reports                 \tab IlluminaHiSeq_DNASeq_Cont_automated\cr
-#'Genome_Wide_SNP_6                 \tab bio                            \cr
-#'tissue_images                     \tab Mixed_DNASeq_automated         \cr
-#'HumanMethylation27                \tab Mixed_DNASeq_Cont_curated      \cr
-#'IlluminaHiSeq_RNASeqV2            \tab Mixed_DNASeq_Cont
-#'}
-#' @param level '1' '2' '3'
-#' @param added.since 04- 14-2010
-#' @param added.up.to 04- 14-2010
-#' @param center center name
-#' @param samples List of samples. Ex:c('TCGA-04-06-*','TCGA-04-08-*')
-#' @example inst/examples/tcgaSearch.R
-#' @export
-#' @importFrom downloader download
-#' @importFrom knitr kable
-#' @return A dataframe with the results of the query
-#'        (lastest version of the files)
-TCGAQuery <- function(tumor = NULL, platform = NULL, added.since = NULL,
-                      added.up.to = NULL, samples = NULL, center = NULL,
-                      level = NULL) {
-
-  disease.table   <- get("disease.table")
-  platform.table  <- get("platform.table")
-  center.table  <- get("center.table")
-  db <-  get("tcga.db")
-
-  if (!is.null(tumor)) {
-    sapply(tumor, function(x){
-      if (!(is.element(tolower(x),
-                       tolower(disease.table$abbreviation)))) {
-        suppressWarnings(
-          df <- as.data.frame(matrix(sort(unique(disease.table$abbreviation)),
-                                     ncol = 8))
-        )
-        print(kable(df, col.names = NULL, format = "pandoc",
-                    caption = "TCGA tumors"))
-        cat("=======================================================\n")
-        cat("ERROR: Disease not found. Select from the table above.\n")
-        cat("=======================================================\n")
-      }
-      return(NULL)
-    })
-  }
-  if (!is.null(platform)) {
-    sapply(platform, function(x){
-      if (!(is.element(tolower(x), tolower(platform.table$name)))) {
-        suppressWarnings(
-          df <- as.data.frame(matrix(sort(unique(platform.table$name)),
-                                     ncol = 3))
-        )
-        print(kable(df, col.names = NULL, format = "pandoc",
-                    caption = "TCGA Platforms"))
-        cat("=======================================================\n")
-        cat("ERROR: Platform not found. Select from the table above.\n")
-        cat("=======================================================\n")
-        return(NULL)
-      }
-    })}
-
-  if (!is.null(center)) {
-    if (!(is.element(tolower(center), tolower(center.table$name)))) {
-      suppressWarnings(
-        df <- as.data.frame(matrix(sort(unique(center.table$name)),
-                                   ncol = 3))
-      )
-      print(kable(df, col.names = NULL, format = "pandoc",
-                  caption = "TCGA Centers"))
-      cat("=======================================================\n")
-      cat("ERROR: Center not found. Select from the table above.\n")
-      cat("=======================================================\n")
-      return(NULL)
-    }
-  }
-  if (!is.null(level)) {
-    if (!(is.element(level, c("1", "2", "3","mage-tab")))) {
-      message("Level not found. Chosse between:'1', '2','3','mage-tab'")
-      stop("Invalid platform")
-    }
-  }
-
-  if (!is.null(added.since)) {
-    d <- try(as.Date(added.since, format = "%Y/%m/%d"))
-    if (class(d) == "try-error" || is.na(d)) {
-      print("Date format should be YYYY-mm-dd")
-    }
-  }
-  if (!is.null(added.up.to)) {
-    d <- try(as.Date(added.up.to, format = "%Y/%m/%d"))
-    if (class(d) == "try-error" || is.na(d)) {
-      print("Date format should be YYYY-mm-dd")
-    }
-  }
-
-
-  if(!is.null(tumor)){
-    id <- sapply(tumor, function(x){
-      grepl(x, db$Disease, ignore.case = TRUE)
-    })
-    id <- apply(id, 1,any)
-    db <-  db[id,]
-  }
-  if(!is.null(platform)){
-    id <- sapply(platform, function(x){
-      grepl(x, db$Platform, ignore.case = TRUE)
-    })
-    id <- apply(id, 1,any)
-    db <-  db[id,]
-  }
-  if(!is.null(center)){
-    id <- sapply(center, function(x){
-      grepl(x, db$Center, ignore.case = TRUE)
-    })
-    id <- apply(id, 1,any)
-    db <-  db[id,]
-  }
-  if(!is.null(level)){
-    id <- grep(paste0("Level_", level), db$name)
-    if(length(id) > 0){
-      db <-  db[id,]
-    }
-  }
-
-  if (!is.null(added.since)) {
-    db <- subset(db, as.Date(db$addedDate) > as.Date(added.since,
-                                                     "%m/%d/%Y"))
-  }
-  if (!is.null(added.up.to)) {
-    db <- subset(db, as.Date(db$addedDate) < as.Date(added.up.to,
-                                                     "%m/%d/%Y"))
-  }
-
-  # to be improved
-  idx <- c()
-  if(!is.null(samples)){
-    for(i in seq_along(samples)){
-      aux <- grep(samples[i],db$barcode)
-      idx <- union(idx, aux)
-    }
-    db <- db[idx,]
-  }
-  return(db)
 }
 
