a307cb660b8ec9e93bcd8ea58bab10e6d0ed2095
Tiago Chedraoui Silva <tiagochst@gmail.com>
2015-05-20 09:25:58 -0300

    Updating demo and vignettes
    Former-commit-id: 544cf04c81e2d1fb0e5f43faf426312978d90645

--- demo/demo2.R
+++ demo/demo2.R
@@ -10,10 +10,11 @@ samples <- c("TCGA-06-0125-01A-01D-A45W-05","TCGA-06-0152-01A-02D-A45W-05",
              "TCGA-06-0221-02A-11D-2004-05","TCGA-14-0736-02A-01D-2004-05",
              "TCGA-19-0957-02A-11D-2004-05","TCGA-19-1389-02A-21D-2004-05",
              "TCGA-14-1402-02A-01D-2004-05","TCGA-19-4065-02A-11D-2004-05")
-query <- tcgaQuery(tumor = "gbm", platform = "HumanMethylation450",level="3",listSample = samples)
-tcgaDownload(query,path="data")
-query <- tcgaQuery(tumor = "gbm", platform = "bio", level="2")
-tcgaDownload(query,path="data")
+query <- TCGAQuery(tumor = "gbm", platform = "HumanMethylation450",
+                   level = "3", samples = samples)
+TCGADownload(query,path = "dataDemo2")
+query <- TCGAQuery(tumor = "gbm", platform = "bio", level = "2")
+TCGADownload(query, path = "dataDemo2")
 
 #-------------------------- Preparing data and metadata
 # met: data frame with probe info (probe id, ge) and beta values
@@ -21,11 +22,11 @@ tcgaDownload(query,path="data")
 # probe: data frame with only probe info(probe vs patient)
 # Conclusion: met = probe +d beta
 # met.md = patient metadata
-met <- organizeMethylationDataFrame("data")
+met <- organizeMethylationDataFrame("dataDemo2")
 probe  <- met[,1:4]
 beta   <- met[,5:ncol(met)]
 
-met.md <- organizeMethylationMetaDataFrame("data")
+met.md <- organizeMethylationMetaDataFrame("dataDemo2")
 samples <- colnames(beta)
 idx <- is.element(met.md$bcr_patient_barcode,strtrim(samples,12))
 met.md <- met.md[idx,]
@@ -41,4 +42,4 @@ probe$p.value <- pvalues[,1]
 probe$p.value.adj <- pvalues[,2]
 prim.rec <- diffmean(beta[1:2,], beta[3:4,])
 probe <- cbind(probe,prim.rec)
-hypo.hyper <- volcano.plot(probe,p.cut=0.05,diffmean.cut = 0.2)
+hypo.hyper <- volcanoPlot(probe,p.cut = 0.05,diffmean.cut = 0.2)
