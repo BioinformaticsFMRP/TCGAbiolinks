e64aac102f49dcc09859ddfe22de3a3aa4d0a3c7
Tiago Chedraoui Silva <tiagochst@gmail.com>
2015-05-21 15:04:38 -0300

    Improving code style to bioconductor

--- R/TCGAInternal.R
+++ R/TCGAInternal.R
@@ -3,38 +3,40 @@
 # @keywords internal
 .onAttach <- function (libname, pkgname){
 
-  file = system.file("extdata/GRCh.rda",package = "TCGAbiolinks")
-  load(file,envir = as.environment("package:TCGAbiolinks"))
+    file = system.file("extdata/GRCh.rda",package = "TCGAbiolinks")
+    load(file,envir = as.environment("package:TCGAbiolinks"))
 
-  file = system.file("extdata/dataGeneExpression.rda",package = "TCGAbiolinks")
-  load(file,envir = as.environment("package:TCGAbiolinks"))
+    file = system.file("extdata/dataGeneExpression.rda",
+                       package = "TCGAbiolinks")
+    load(file,envir = as.environment("package:TCGAbiolinks"))
 
-  file = system.file("extdata/dataEnrichmentAnalysis.rda",package = "TCGAbiolinks")
-  load(file,envir = as.environment("package:TCGAbiolinks"))
+    file = system.file("extdata/dataEnrichmentAnalysis.rda",
+                       package = "TCGAbiolinks")
+    load(file,envir = as.environment("package:TCGAbiolinks"))
 
-  file = system.file("extdata/dataFolders.rda",package = "TCGAbiolinks")
-  time <- file.info(file)$ctime
-  if (file.exists(file)) {
-    load(file, envir = as.environment("package:TCGAbiolinks"))
-  } else {
-    env <- as.environment("package:TCGAbiolinks")
-    load.tcga(env)
-  }
+    file = system.file("extdata/dataFolders.rda",package = "TCGAbiolinks")
+    time <- file.info(file)$ctime
+    if (file.exists(file)) {
+        load(file, envir = as.environment("package:TCGAbiolinks"))
+    } else {
+        env <- as.environment("package:TCGAbiolinks")
+        load.tcga(env)
+    }
 
-  if (!interactive() || stats::runif(1) > 0.1) return()
-  welcome.message <- paste0(
-    " =============================================================\n",
-    " ______  ___  ____   ___                                        \n",
-    "   ||   |    |      |   | |    o  __  |   o  _         __         \n",
-    "   ||   |    | ___  |___| |__  | |  | |   | | | | |_/ |__         \n",
-    "   ||   |___ |____| |   | |__| | |__| |__ | | |_| | \\  __|       \n",
-    " ------------------------------------------------------------\n",
-    " Search, download & analyse - TCGA                  \n",
-    " Version:",utils::packageVersion("TCGAbiolinks"),"\n",
-    " Last TCGAUpdate(): ",time,"\n",
-    " ==============================================================\n"
-  )
-  packageStartupMessage(welcome.message)
+    if (!interactive() || stats::runif(1) > 0.1) return()
+    welcome.message <- paste0(
+        " =============================================================\n",
+        " ______  ___  ____   ___                                        \n",
+        "   ||   |    |      |   | |    o  __  |   o  _         __         \n",
+        "   ||   |    | ___  |___| |__  | |  | |   | | | | |_/ |__         \n",
+        "   ||   |___ |____| |   | |__| | |__| |__ | | |_| | \\  __|       \n",
+        " ------------------------------------------------------------\n",
+        " Search, download & analyse - TCGA                  \n",
+        " Version:",utils::packageVersion("TCGAbiolinks"),"\n",
+        " Last TCGAUpdate(): ",time,"\n",
+        " ==============================================================\n"
+    )
+    packageStartupMessage(welcome.message)
 
 }
 
@@ -45,128 +47,128 @@
 #' @importFrom downloader download
 #' @keywords internal
 load.tcga <- function(env) {
-  tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
+    tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
 
-  # Get platform table
-  tcga.query <- "query=Platform"
-  next.url <- paste0(tcga.root, tcga.query)
-  platform.table <- tcgaGetTable(next.url)
-  platform.table <- platform.table[, 1:4]
-  platform.table <- platform.table[order(platform.table$name,
-                                         decreasing = TRUE),]
+    # Get platform table
+    tcga.query <- "query=Platform"
+    next.url <- paste0(tcga.root, tcga.query)
+    platform.table <- tcgaGetTable(next.url)
+    platform.table <- platform.table[, 1:4]
+    platform.table <- platform.table[order(platform.table$name,
+                                           decreasing = TRUE),]
 
-  # Get disease table
-  tcga.query <- "query=Disease"
-  next.url <- paste0(tcga.root, tcga.query)
-  disease.table <- tcgaGetTable(next.url)
-  disease.table <- disease.table[, 1:3]
+    # Get disease table
+    tcga.query <- "query=Disease"
+    next.url <- paste0(tcga.root, tcga.query)
+    disease.table <- tcgaGetTable(next.url)
+    disease.table <- disease.table[, 1:3]
 
-  # Get center table
-  tcga.query <- "query=Center"
-  next.url <- paste0(tcga.root, tcga.query)
-  center.table  <- tcgaGetTable(next.url)
-  center.table <- center.table[, 1:3]
+    # Get center table
+    tcga.query <- "query=Center"
+    next.url <- paste0(tcga.root, tcga.query)
+    center.table  <- tcgaGetTable(next.url)
+    center.table <- center.table[, 1:3]
 
-  env$platform.table <- platform.table
-  env$center.table <- center.table
-  env$disease.table <- disease.table
+    env$platform.table <- platform.table
+    env$center.table <- center.table
+    env$disease.table <- disease.table
 
-  # Get tcga folders with barcodes without private folders
-  tcga.db <- createTcgaTable()
-  env$tcga.db <- tcga.db
-  save(tcga.db, paste0(system.file("extdata", package = "TCGAbiolinks"),
-                      "/tcgadb.rda"))
-  step <- 200
-  for(i in seq(1, nrow(tcga.db) , by = step)){
-    print(i)
-    j <- i + step
-    if(j > nrow(tcga.db)){
-      j <-nrow(tcga.db)
-    }
-    tcga.db[i:j,]$deployStatus <- getBarcode(tcga.db[i:j,])$barcode
+    # Get tcga folders with barcodes without private folders
+    tcga.db <- createTcgaTable()
+    env$tcga.db <- tcga.db
     save(tcga.db, paste0(system.file("extdata", package = "TCGAbiolinks"),
                          "/tcgadb.rda"))
-  }
+    step <- 200
+    for(i in seq(1, nrow(tcga.db) , by = step)){
+        print(i)
+        j <- i + step
+        if(j > nrow(tcga.db)){
+            j <-nrow(tcga.db)
+        }
+        tcga.db[i:j,]$deployStatus <- getBarcode(tcga.db[i:j,])$barcode
+        save(tcga.db, paste0(system.file("extdata", package = "TCGAbiolinks"),
+                             "/tcgadb.rda"))
+    }
 
-  env$tcga.db <- tcga.db
-  save(platform.table, disease.table, tcga.db, center.table,
-       file = paste0(system.file("extdata", package = "TCGAbiolinks"),
-                     "/dataFolders.rda")
-  )
+    env$tcga.db <- tcga.db
+    save(platform.table, disease.table, tcga.db, center.table,
+         file = paste0(system.file("extdata", package = "TCGAbiolinks"),
+                       "/dataFolders.rda")
+    )
 }
 
 # Get all files in the ftp directory @keywords internal
 getFileNames <- function(url) {
 
-  download(url,
-           "temp.html",
-           mode = "wb",
-           quiet = 1)
-  x <- capture.output(XML::htmlTreeParse("temp.html"))
-  unlink("temp.html")
-  x <- x[grep("href", x)]
-  if (is.null(x)){
-    return(NULL)
-  }
-  x = sapply(strsplit(x, ">"), function(y) y[2])
-  if (is.null(x)){
-    return(NULL)
-  }
-  x = sapply(strsplit(x, "<"), function(y) y[1])
-  return(x)
+    download(url,
+             "temp.html",
+             mode = "wb",
+             quiet = 1)
+    x <- capture.output(XML::htmlTreeParse("temp.html"))
+    unlink("temp.html")
+    x <- x[grep("href", x)]
+    if (is.null(x)){
+        return(NULL)
+    }
+    x = sapply(strsplit(x, ">"), function(y) y[2])
+    if (is.null(x)){
+        return(NULL)
+    }
+    x = sapply(strsplit(x, "<"), function(y) y[1])
+    return(x)
 }
 
 tcga.get.barcode <- function(data){
-  # todo considere next page =/
-  # comparar com sdrf
-  # for each tcga.db id get barcodes
-  message("Downloading TCGA barcodes")
-  all.barcode <- c()
+    # todo considere next page =/
+    # comparar com sdrf
+    # for each tcga.db id get barcodes
+    message("Downloading TCGA barcodes")
+    all.barcode <- c()
 
-  tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?query="
-  tcga.query <- paste0("FileInfo&Archive[@id=",data$id,"]&roleName=fileCollection")
+    tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?query="
+    tcga.query <- paste0("FileInfo&Archive[@id=",data$id,
+                         "]&roleName=fileCollection")
 
-  next.url <- paste0(tcga.root,tcga.query)
-  files <- tcgaGetTable(next.url)
-  #print(files)
-  if(nrow(files) == 0){
-    return (NULL)
-  }
-  files <- files[,1:4]
-  idx <- grep("somatic.maf",files$name)
-  if (length(idx > 0)) {
-    files <- files[idx,]
-  }
-  else {
-    # no maf file
-    # try in the name
-    #pat <- "*(TCGA)-([A-Z0-9]{2})-([A-Z0-9]{4})-(0[1-9]|[1-2][0-9])([A-Z])-(0[1-9]|[1-9][0-9])([DGHRTWX])-([A-Z0-9]{4})-([A-Z0-9]{2})*"
-    #pat <- "TCGA-[A-Z0-9]{2}-[A-Z0-9]{4}-[A-Z0-9]{3}-[A-Z0-9]{3}-[A-Z0-9]{4}-[A-Z0-9]{2}"
-    pat <- "((((TCGA-[A-Z0-9]{2})-[A-Z0-9]{4})-[A-Z0-9]{3}-[A-Z0-9]{3})-[A-Z0-9]{4}-[A-Z0-9]{2})"
-    barcode <- str_match(files$name,pat)[,1]
-    #message("Found in name")
-    if(!all(is.na(barcode))){
-      message("Found in name")
-      barcode <- barcode[!is.na(barcode)]
-      barcode <- paste0(unique(barcode), collapse = ",")
-      return (barcode)
+    next.url <- paste0(tcga.root,tcga.query)
+    files <- tcgaGetTable(next.url)
+    #print(files)
+    if (nrow(files) == 0) {
+        return(NULL)
+    }
+    files <- files[,1:4]
+    idx <- grep("somatic.maf",files$name)
+    if (length(idx > 0)) {
+        files <- files[idx,]
+    }
+    else {
+        # no maf file
+        # try in the name
+        pat <- paste0("((((TCGA-[A-Z0-9]{2})-[A-Z0-9]{4})-[A-Z0-9]{3}-",
+                      "[A-Z0-9]{3})-[A-Z0-9]{4}-[A-Z0-9]{2})")
+        barcode <- str_match(files$name,pat)[,1]
+        #message("Found in name")
+        if (!all(is.na(barcode))) {
+            message("Found in name")
+            barcode <- barcode[!is.na(barcode)]
+            barcode <- paste0(unique(barcode), collapse = ",")
+            return(barcode)
+        }
+
+        return(NULL)
     }
 
-    return(NULL)
-  }
+    # maybe two maf files
+    for (i in  seq_along(files$name)) {
+        tcga.query <- paste0("BiospecimenBarcode&FileInfo[@id=",files[i,"id"],
+                             "]&roleName=biospecimenBarcodeCollection")
+        next.url <- paste0(tcga.root,tcga.query)
+        print(next.url)
+        barcode.table <- tcgaGetTable(next.url)
+        barcode.table <- barcode.table[,1:8]
+        all.barcode <- union(all.barcode, unique(barcode.table$barcode))
+    }
 
-  # maybe two maf files
-  for (i in  seq_along(files$name)) {
-    tcga.query <- paste0("BiospecimenBarcode&FileInfo[@id=",files[i,"id"],
-                         "]&roleName=biospecimenBarcodeCollection")
-    next.url <- paste0(tcga.root,tcga.query)
-    print(next.url)
-    barcode.table <- tcgaGetTable(next.url)
-    barcode.table <- barcode.table[,1:8]
-    all.barcode <- union(all.barcode, unique(barcode.table$barcode))
-  }
+    all.barcode <- paste0(unique(all.barcode), collapse = ",")
 
-  all.barcode <- paste0(unique(all.barcode), collapse = ",")
-
-  return(all.barcode)
+    return(all.barcode)
 }
--- R/clinical.R
+++ R/clinical.R
@@ -6,18 +6,19 @@
 #' @export
 #' @return SampleTypes
 SampleTypes <- function(barcode, typesample){
-  table.code <- c('01','02','03','04','05','06','07','08','09','10','11',
-                  '12','13','14','20','40','50','60','61')
-  names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
-                         "TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB",
-                         "CELL","XP","XCL")
+    table.code <- c('01','02','03','04','05','06','07','08','09','10','11',
+                    '12','13','14','20','40','50','60','61')
+    names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
+                           "TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB",
+                           "CELL","XP","XCL")
 
-  if(is.element(typesample,names(table.code))){
-    barcode <- barcode[grep(table.code[typesample], substr(barcode, 14, 15))]
-  }else{
-    return("Error message: sample type does not exist")
-  }
-  return(barcode)
+    if(is.element(typesample,names(table.code))){
+        barcode <- barcode[grep(table.code[typesample],
+                                substr(barcode, 14, 15))]
+    }else{
+        return("Error message: sample type does not exist")
+    }
+    return(barcode)
 }
 
 #' @title MultiSampleTypes
@@ -31,25 +32,25 @@ SampleTypes <- function(barcode, typesample){
 #' samplesNT <- MultiSampleTypes(colnames(dataFilt), typesample = c("NT"))
 #' @return MultiSampleTypes
 MultiSampleTypes <- function(barcode,typesample){
-  # Tumor AND Solid Tissue Normal NOT FROM THE SAME PATIENTS
-  table.code <- c('01','02','03','04','05','06','07','08','09','10',
-                  '11','12','13','14','20','40','50','60','61')
-  names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
-                         "TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB",
-                         "CELL","XP","XCL")
+    # Tumor AND Solid Tissue Normal NOT FROM THE SAME PATIENTS
+    table.code <- c('01','02','03','04','05','06','07','08','09','10',
+                    '11','12','13','14','20','40','50','60','61')
+    names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
+                           "TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB",
+                           "CELL","XP","XCL")
 
-  if(sum(is.element(typesample,names(table.code))) == length(typesample)) {
+    if(sum(is.element(typesample,names(table.code))) == length(typesample)) {
 
-    string <- substr(barcode, 14, 15)
-    barcode.all <- NULL
-    for(sample.i in typesample){
-      barcode.all <- union(barcode.all,
-                           barcode[grep(table.code[sample.i], string)])
+        string <- substr(barcode, 14, 15)
+        barcode.all <- NULL
+        for(sample.i in typesample){
+            barcode.all <- union(barcode.all,
+                                 barcode[grep(table.code[sample.i], string)])
+        }
+        return(barcode.all)
+    }else{
+        return("Error message: one or more sample types do not exist")
     }
-    return(barcode.all)
-  }else{
-    return("Error message: one or more sample types do not exist")
-  }
 }
 
 #' @title MatchedCoupledSampleTypes
@@ -60,33 +61,34 @@ MultiSampleTypes <- function(barcode,typesample){
 #' @export
 #' @return MultiSampleTypes
 MatchedCoupledSampleTypes <- function(barcode,typesample){
-  # Tumor AND Solid Tissue Normal FROM THE SAME PATIENTS
-  table.code <- c('01','02','03','04','05','06','07','08','09','10',
-                  '11','12','13','14','20','40','50','60','61')
-  names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
-                         "TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB",
-                         "CELL","XP","XCL")
-  if(length(typesample)!=2){
-    return("Error message: exactly two types need to be provided")
-  }
-
-  if(sum(is.element(typesample,names(table.code))) == length(typesample)) {
-
-    string <- substr(barcode, 14, 15)
-    barcode.1 <- barcode[grep(table.code[typesample[1]], string)]
-    barcode.2 <- barcode[grep(table.code[typesample[2]], string)]
-
-
-    barcode.common <- intersect(substr(barcode.1,1,13), substr(barcode.2,1,13))
-    if(length(barcode.common) > 0){
-      return(union(barcode.1[grep(barcode.common,barcode.1)],
-                   barcode.2[grep(barcode.common,barcode.2)]))
-    }else{
-      return("Error message: there exist no matched samples")
+    # Tumor AND Solid Tissue Normal FROM THE SAME PATIENTS
+    table.code <- c('01','02','03','04','05','06','07','08','09','10',
+                    '11','12','13','14','20','40','50','60','61')
+    names(table.code) <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
+                           "TBM","NB","NT","NBC","NEBV","NBM","CELLC","TRB",
+                           "CELL","XP","XCL")
+    if(length(typesample)!=2){
+        return("Error message: exactly two types need to be provided")
     }
-  } else{
-    return("Error message: one or more sample types do not exist")
-  }
+
+    if(sum(is.element(typesample,names(table.code))) == length(typesample)) {
+
+        string <- substr(barcode, 14, 15)
+        barcode.1 <- barcode[grep(table.code[typesample[1]], string)]
+        barcode.2 <- barcode[grep(table.code[typesample[2]], string)]
+
+
+        barcode.common <- intersect(substr(barcode.1,1,13),
+                                    substr(barcode.2,1,13))
+        if(length(barcode.common) > 0){
+            return(union(barcode.1[grep(barcode.common,barcode.1)],
+                         barcode.2[grep(barcode.common,barcode.2)]))
+        }else{
+            return("Error message: there exist no matched samples")
+        }
+    } else{
+        return("Error message: one or more sample types do not exist")
+    }
 }
 
 #' @title stage_BRCA
@@ -98,27 +100,28 @@ MatchedCoupledSampleTypes <- function(barcode,typesample){
 #' @export
 #' @return stage_BRCA
 stage_BRCA <- function(barcode, stage, clinical_patient_data){
-  table.stages <- c("Stage I$|Stage IA$|Stage IB$", "Stage I$", "Stage IA$",
-                    "Stage IB$", "Stage II$|Stage IIA$|Stage IIB$",
-                    "Stage II$", "Stage IIA$", "Stage IIB$",
-                    "Stage III$|Stage IIIA$|Stage IIIB$|Stage IIIC$",
-                    "Stage III$", "Stage IIIA$", "Stage IIIB$",
-                    "Stage IIIC$", "Stage IV$")
-  names(table.stages) <- c("stage_IX", "stage_I", "stage_IA", "stage_IB",
-                           "stage_IIX", "stage_IIA", "stage_IIB", "stage_IIIX",
-                           "stage_IIIA", "stage_IIIB", "stage_IIIC", "stage_IV")
+    table.stages <- c("Stage I$|Stage IA$|Stage IB$", "Stage I$", "Stage IA$",
+                      "Stage IB$", "Stage II$|Stage IIA$|Stage IIB$",
+                      "Stage II$", "Stage IIA$", "Stage IIB$",
+                      "Stage III$|Stage IIIA$|Stage IIIB$|Stage IIIC$",
+                      "Stage III$", "Stage IIIA$", "Stage IIIB$",
+                      "Stage IIIC$", "Stage IV$")
+    names(table.stages) <- c("stage_IX", "stage_I", "stage_IA", "stage_IB",
+                             "stage_IIX", "stage_IIA", "stage_IIB",
+                             "stage_IIIX","stage_IIIA", "stage_IIIB",
+                             "stage_IIIC", "stage_IV")
 
-  if(is.element(stage, names(table.stages))) {
-    clinical_patient_data<-as.data.frame(clinical_patient_data)
-    stage.i <- clinical_patient_data[grep(table.stages[stage],
-               clinical_patient_data$ajcc_pathologic_tumor_stage), ]
-    stage.i <- stage.i[,"bcr_patient_barcode"]
-    samples <- substr(barcode, 1, 12)
-    barcode <- intersect(samples,stage.i)
-  }else{
-    return("Error message: stage does not exist")
-  }
-  return(barcode)
+    if (is.element(stage, names(table.stages))) {
+        clinical_patient_data <- as.data.frame(clinical_patient_data)
+        stage.i <- clinical_patient_data[grep(table.stages[stage],
+                  clinical_patient_data$ajcc_pathologic_tumor_stage), ]
+        stage.i <- stage.i[,"bcr_patient_barcode"]
+        samples <- substr(barcode, 1, 12)
+        barcode <- intersect(samples,stage.i)
+    }else{
+        return("Error message: stage does not exist")
+    }
+    return(barcode)
 }
 
 #' @title gender_BRCA
@@ -131,19 +134,19 @@ stage_BRCA <- function(barcode, stage, clinical_patient_data){
 #' @return stage_BRCA
 gender_BRCA <- function(barcode, gender, clinical_patient_data){
 
-  if(is.element(gender,c("MALE", "FEMALE"))){
-    clinical_patient_data<-as.data.frame(clinical_patient_data)
-    s.gender <- as.data.frame(clinical_patient_data)[
-        grep(paste0("^", gender,"$"), clinical_patient_data$gender),
-        ][,"bcr_patient_barcode"]
-    samples<-substr(barcode, 1, 12)
-    #find common patients between FEMALE and barcode data
-    barcode<-intersect(samples,s.gender)
-  }else{
-    return("Error message gender doesn't exist")
-  }
+    if (is.element(gender,c("MALE", "FEMALE"))) {
+        clinical_patient_data <- as.data.frame(clinical_patient_data)
+        s.gender <- as.data.frame(clinical_patient_data)[
+            grep(paste0("^", gender,"$"), clinical_patient_data$gender),
+            ][,"bcr_patient_barcode"]
+        samples <- substr(barcode, 1, 12)
+        #find common patients between FEMALE and barcode data
+        barcode <- intersect(samples,s.gender)
+    }else{
+        return("Error message gender doesn't exist")
+    }
 
-  return(barcode)
+    return(barcode)
 }
 
 #' @title ER_status_BRCA
@@ -155,18 +158,18 @@ gender_BRCA <- function(barcode, gender, clinical_patient_data){
 #' @export
 #' @return ER_status_BRCA
 ER_status_BRCA <- function(barcode,ER, clinical_patient_data){
-  ## ER should be "Positive" or "Negative"
-  # consider only barcode and ER status
-  if(is.element(ER, c("Positive", "Negative"))){
-    status <- as.data.frame(clinical_patient_data)[grep(paste0("^",ER,"$"),
+    ## ER should be "Positive" or "Negative"
+    # consider only barcode and ER status
+    if (is.element(ER, c("Positive", "Negative"))) {
+        status <- as.data.frame(clinical_patient_data)[grep(paste0("^",ER,"$"),
             clinical_patient_data$er_status_by_ihc), ][,"bcr_patient_barcode"]
-    samples <- substr(barcode, 1, 12)
-    #find common patients between ER status and barcode data
-    barcode <- intersect(samples,status)
-    return(barcode)
-  }else{
-    return("Error message: ER status does not exist")
-  }
+        samples <- substr(barcode, 1, 12)
+        #find common patients between ER status and barcode data
+        barcode <- intersect(samples,status)
+        return(barcode)
+    }else{
+        return("Error message: ER status does not exist")
+    }
 }
 
 #' @title PR_status_BRCA
@@ -178,20 +181,21 @@ ER_status_BRCA <- function(barcode,ER, clinical_patient_data){
 #' @export
 #' @return PR_status_BRCA
 PR_status_BRCA  <- function(barcode,PR, clinical_patient_data){
-  ## PR should be "Positive" or "Negative"
+    ## PR should be "Positive" or "Negative"
 
-  if(is.element(PR, c("Positive", "Negative"))){
-    #for breast cancer
-    status <- as.data.frame(clinical_patient_data)[grep(paste0("^", PR, "$"),
+    if(is.element(PR, c("Positive", "Negative"))){
+        #for breast cancer
+        status <- as.data.frame(clinical_patient_data)[grep(paste0("^", PR, "$"),
             clinical_patient_data$pr_status_by_ihc), ][,"bcr_patient_barcode"]
-    samples<-substr(barcode, 1, 12)
-    #find common patients between PR status and barcode data
-    barcode<-intersect(samples,status)
-  }else{
-    return("Error message: PR status does not exist")
-  }
 
-  return(barcode)
+        samples <- substr(barcode, 1, 12)
+        #find common patients between PR status and barcode data
+        barcode <- intersect(samples,status)
+    }else{
+        return("Error message: PR status does not exist")
+    }
+
+    return(barcode)
 
 }
 
@@ -204,18 +208,18 @@ PR_status_BRCA  <- function(barcode,PR, clinical_patient_data){
 #' @export
 #' @return HER_status_BRCA
 HER_status_BRCA  <- function(barcode, HER, clinical_patient_data){
-  if(is.element(HER, c("Positive", "Negative"))){
-    clinical_patient_data<-as.data.frame(clinical_patient_data)
-    #for breast cancer HER+
-    status <- as.data.frame(clinical_patient_data)[grep(paste0("^",HER,"$"),
-          clinical_patient_data$her2_status_by_ihc), ][,"bcr_patient_barcode"]
-    samples<-substr(barcode, 1, 12)
-    #find common patients between HER+ e barcode data
-    barcode<-intersect(samples,status)
-    return(barcode)
-  }else{
-    return("Error message: HER status does not exist")
-  }
+    if (is.element(HER, c("Positive", "Negative"))) {
+        clinical_patient_data <- as.data.frame(clinical_patient_data)
+        #for breast cancer HER+
+        status <- as.data.frame(clinical_patient_data)[grep(paste0("^",HER,"$"),
+            clinical_patient_data$her2_status_by_ihc), ][,"bcr_patient_barcode"]
+        samples <- substr(barcode, 1, 12)
+        #find common patients between HER+ e barcode data
+        barcode <- intersect(samples,status)
+        return(barcode)
+    }else{
+        return("Error message: HER status does not exist")
+    }
 
 
 }
@@ -227,10 +231,10 @@ HER_status_BRCA  <- function(barcode, HER, clinical_patient_data){
 #' @export
 #' @examples clinical_data_site_cancer("gbm")
 #' @return clinical_data_site_cancer
-clinical_data_site_cancer <-function(cancer){
-  return(paste0("https://tcga-data.nci.nih.gov/tcgafiles/",
-                "ftp_auth/distro_ftpusers/anonymous/tumor/",
-                cancer,"/bcr/biotab/clin/"))
+clinical_data_site_cancer <- function(cancer){
+    return(paste0("https://tcga-data.nci.nih.gov/tcgafiles/",
+                  "ftp_auth/distro_ftpusers/anonymous/tumor/",
+                  cancer,"/bcr/biotab/clin/"))
 }
 
 #' @title clinic
@@ -244,10 +248,10 @@ clinical_data_site_cancer <-function(cancer){
 #' @return clinic
 clinic <- function(cancer,clinical_data_type){
 
-  URL <- paste0(clinical_data_site_cancer (cancer), "nationwidechildrens.org_",
-                clinical_data_type, "_", cancer, ".txt")
-  writeLines(getURL(URL,ssl.verifypeer = FALSE),
-             file(paste0(clinical_data_type,".txt")))
+    URL <- paste0(clinical_data_site_cancer(cancer), "nationwidechildrens.org_",
+                  clinical_data_type, "_", cancer, ".txt")
+    writeLines(getURL(URL,ssl.verifypeer = FALSE),
+               file(paste0(clinical_data_type,".txt")))
 
-  return(file(paste0(clinical_data_type,".txt")))
+    return(file(paste0(clinical_data_type,".txt")))
 }
