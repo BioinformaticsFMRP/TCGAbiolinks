9650010ba7b1695b534fa43d82178d22384cf1e8
torongs82 <antonio.colaprico@gmail.com>
2015-06-14 19:08:36 +0200

    Update in Vignette TCGA integrate and checking in Clinical data

---
+++ R/TCGAintegrate.R
@@ -0,0 +1,23 @@
+#' @title TCGAintegrate
+#' @description Common samples among platforms with 2 by 2 comparison
+#' @param querySamples output of TCGAQuery filtered
+#' @export
+#' @return table with common samples among platforms from TCGAQuery
+TCGAintegrate<- function(querySamples){
+
+    matSamples <- matrix(0,length(querySamples),length(querySamples))
+    colnames(matSamples) <- names(querySamples)
+    rownames(matSamples) <- names(querySamples)
+
+    for ( i in 1: nrow(matSamples)){
+        for( j in 1: nrow(matSamples)){
+            if(i!=j){ matSamples[i,j] <- length(intersect(substr(querySamples[[i]],1,12), substr(querySamples[[j]],1,12)))}
+        else{ matSamples[i,j] <- length(intersect(querySamples[[i]], querySamples[[j]]))}
+        }
+
+    }
+
+
+
+    return(matSamples)
+}
