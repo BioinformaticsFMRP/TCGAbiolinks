1acac39fbaadacd6d6e2a0444307cad3e464f5a2
torongs82 <antonio.colaprico@gmail.com>
2015-06-18 18:50:29 +0200

    Removed TCGAVersion2 and related PlatformAndAssociatedData
    Former-commit-id: 8fd3d468b391eb31a2b6b22380aff36be8ae22f8

--- R/TCGAbiolinks.R
+++ R/TCGAbiolinks.R
@@ -32,14 +32,6 @@ NULL
 #' @format A data frame with 1157 rows and 2 variables
 NULL
 
-#' TCGA PlatformAndAssociatedData data frame indicating the different
-#' characteristics of the data e.g. tumour, type, species.
-#' @docType data
-#' @keywords internal
-#' @name PlatformAndAssociatedData
-#' @format A data frame with 615 rows and 12 variables
-NULL
-
 #' TCGA Table with version, number of samples and size (Mbyte) of
 #' BRCA IlluminaHiSeq_RNASeqV2 Level 3
 #' @docType data
