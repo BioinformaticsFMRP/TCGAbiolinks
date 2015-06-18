8831de48ac70be52c4d5ce268acc6b67f130dc3a
tiago <tiagochst@gmail.com>
2015-06-18 00:19:35 -0300

    * Checking exported objects have runnable examples...     * REQUIRED: At least 80% of man pages documenting exported objects       must have runnable examples.The following pages do not:       CreateTabLevel.Rd, DEArnaSEQ.Rd, EAbarplot.Rd, EAcomplete.Rd,   EnrichmentAnalysis.Rd, GeneSplitRegulon.Rd, plotPCAforGroups.Rd,   RnaSeqFilt.Rd, RnaSeqNormalization.Rd

--- R/TCGAintegrate.R
+++ R/TCGAintegrate.R
@@ -3,6 +3,10 @@
 #' @param querySamples output of TCGAQuery filtered
 #' @export
 #' @return table with common samples among platforms from TCGAQuery
+#' @examples
+#' TCGAintegrate(c("TCGA-A7-A0D9-01A-31D-A060-02",
+#'                 "TCGA-B6-A0RG-01A-11D-A060-02",
+#'                  "TCGA-B6-A0RG-01A-11D-A060-01"))
 TCGAintegrate<- function(querySamples){
 
     matSamples <- matrix(0,length(querySamples),length(querySamples))
@@ -11,13 +15,13 @@ TCGAintegrate<- function(querySamples){
 
     for ( i in 1: nrow(matSamples)){
         for( j in 1: nrow(matSamples)){
-            if(i!=j){ matSamples[i,j] <- length(intersect(substr(querySamples[[i]],1,12), substr(querySamples[[j]],1,12)))}
-        else{ matSamples[i,j] <- length(intersect(querySamples[[i]], querySamples[[j]]))}
+            if(i!=j){ matSamples[i,j] <- length(
+                intersect(substr(querySamples[[i]],1,12),
+                          substr(querySamples[[j]],1,12)))}
+        else{ matSamples[i,j] <- length(intersect(
+            querySamples[[i]], querySamples[[j]]))}
         }
 
     }
-
-
-
     return(matSamples)
 }
