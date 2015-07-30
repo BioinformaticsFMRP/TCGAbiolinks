5c23401494af7a04ca38d455e86585c20807d3dd
tiago <tiagochst@gmail.com>
2015-07-30 15:38:28 -0300

    1 Db update 2 - bugfix in internal function. Else was in another line
    Former-commit-id: 7a393b7fe2718a9e2a670b040eee508e5443541a

--- R/TCGAInternal.R
+++ R/TCGAInternal.R
@@ -130,8 +130,7 @@ tcga.get.barcode <- function(data){
     idx <- grep("somatic.maf",files$name)
     if (length(idx > 0)) {
         files <- files[idx,]
-    }
-    else {
+    } else {
         # no maf file
         # try in the name
         pat <- paste0("((((TCGA-[A-Z0-9]{2})-[A-Z0-9]{4})-[A-Z0-9]{3}-",
