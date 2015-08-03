c1616176daca86202291e2876636aed33023dfd9
torongs82 <antonio.colaprico@gmail.com>
2015-08-03 17:03:14 +0200

    some fix

--- R/TCGAquery_subtypes.R
+++ R/TCGAquery_subtypes.R
@@ -62,11 +62,7 @@ TCGAquery_subtypes <- function(tumor = NULL, path = ".") {
     }
 
     suppressWarnings(
-<<<<<<< HEAD
-        downloader::download(FileSubtypes,filetoDown, quiet = TRUE,mode ="wb")
-=======
         download(FileSubtypes,filetoDown, quiet = TRUE,  mode="wb")
->>>>>>> origin/master
     )
     #table <- read.xlsx2(file,1,stringsAsFactors = NULL)
     return(filetoDown)
