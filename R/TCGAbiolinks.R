7f5c9fc22f039dcac37fe92ab961d5d00b949126
j.hester <j.hester@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-08-11 20:25:47 +0000

    Add ABAEnrichment and TCGAbiolinks
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TCGAbiolinks@107331 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/TCGAbiolinks.R
@@ -0,0 +1,108 @@
+#' Download data of samples from TCGA
+#'
+#' TCGAbiolinks allows you to Download data of samples from TCGA
+#'
+#' The functions you're likely to need from \pkg{TCGAbiolinks} is
+#' \code{\link{TCGAdownload}}, \code{\link{TCGAquery}}.
+#'Otherwise refer to the vignettes to see
+#' how to format the documentation.
+#'
+#' @docType package
+#' @name TCGAbiolinks
+NULL
+
+#' Clinical data TCGA BRCA
+#' @docType data
+#' @keywords internal
+#' @name clinBRCA
+#' @format A data frame with 1061 rows and 109 variables
+NULL
+
+#' tabSurvKMcompleteDEGs
+#' @docType data
+#' @keywords internal
+#' @name tabSurvKMcompleteDEGs
+#' @format A data frame with 200 rows and 7 variables
+NULL
+
+#' BRCA_rnaseqv2
+#' @docType data
+#' @keywords internal
+#' @name BRCA_rnaseqv2
+#' @format A data frame with 200 rows (genes) and 1172 variables (samples)
+NULL
+
+
+#' TCGA Table with version, number of samples and size (Mbyte) of
+#' BRCA IlluminaHiSeq_RNASeqV2 Level 3
+#' @docType data
+#' @keywords internal
+#' @name BRCA_RNASeqV2_version
+#' @format A data frame with 12 rows and 4 variables
+NULL
+
+
+#' tabPackage1
+#' @docType data
+#' @keywords internal
+#' @name tabPackage1
+#' @format A data frame with 40 rows and 3 variables
+NULL
+
+#' tabPackage2
+#' @docType data
+#' @keywords internal
+#' @name tabPackage2
+#' @format A data frame with 40 rows and 3 variables
+NULL
+
+#' tabPackageKey
+#' @docType data
+#' @keywords internal
+#' @name tabPackageKey
+#' @format A data frame with 40 rows and 3 variables
+NULL
+
+#' TCGA data matrix BRCA
+#' @docType data
+#' @keywords internal
+#' @name dataBRCA
+#' @format A data frame with 20531 rows (genes) and 50 variables (samples)
+NULL
+
+#' TCGA data SummarizedExperiment READ
+#' @docType data
+#' @keywords internal
+#' @name dataREAD
+#' @format A SummarizedExperiment of READ with 2 samples
+NULL
+
+#' TCGA data matrix READ
+#' @docType data
+#' @keywords internal
+#' @name dataREAD_df
+#' @format A data frame with 20531 rows (genes) and 2 variables (samples)
+NULL
+
+#' TCGA data matrix BRCA DEGs
+#' @docType data
+#' @keywords internal
+#' @name dataDEGsFiltLevel
+#' @format A data frame with 3649 rows and 6 variables
+NULL
+
+#' geneInfo for normalization
+#' @docType data
+#' @keywords internal
+#' @name geneInfo
+#' @format A data frame with 20531 rows and 2 variables
+NULL
+
+
+#' TCGA data matrix BRCA DEGs Pubmed
+#' @docType data
+#' @keywords internal
+#' @name tabDEGsTFPubmed
+#' @format A data frame with 10 rows and 8 variables
+NULL
+
