36733c79611dccdd88e54ed37a705db0eea3fc13
Antonio Colaprico <antonio.colaprico@ulb.ac.be>
2015-08-13 20:13:53 +0000

    Sample commit
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TCGAbiolinks@107387 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TCGAInternal.R
+++ R/TCGAInternal.R
@@ -161,6 +161,35 @@ tcga.get.barcode <- function(data){
         return(x)
     }
 
+.quantileNormalization <-
+    function(wd, distribution) {
+    n <- nrow(wd)
+    m <- ncol(wd)
+    if(!missing(distribution))
+        if(m != length(distribution))
+            stop("The reference distribution has length
+                 different from the col dimension of the data matrix.") else
+                distribution  <-  sort(distribution)
+
+    o <- matrix(0, n, m)
+    for(i in 1:n)
+        o[i,] <- order(wd[i,])
+
+    j <- 1
+    tmp <- rep(0, n)
+
+    while(j <= m) {
+        if(missing(distribution)) {
+            for(i in 1:n)
+                tmp[i] <- wd[i,o[i,j]]
+            value <- mean(tmp)
+        } else value  <- distribution[j]
+        for(i in 1:n)
+            wd[i,o[i,j]] <- value
+        j <- j + 1
+    }
+    return(wd)
+}
 
 is.windows <- function() {
     Sys.info()["sysname"] == "Windows"
