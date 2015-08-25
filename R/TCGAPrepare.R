324f9b9b08ab064e4efd69c9853a23a36fef3a6c
tiagochst <tiagochst@gmail.com>
2015-08-25 18:10:27 -0300

    fread is giving a bug in 6 probes. Adding the old slow function until it is not solved.

--- R/TCGAPrepare.R
+++ R/TCGAPrepare.R
@@ -135,18 +135,28 @@ TCGAprepare <- function(query,
         barcode <- str_match(files,regex)
 
         for (i in seq_along(files)) {
-            data <- fread(files[i], header = TRUE, sep = "\t",
-                          stringsAsFactors = FALSE,skip = 1,
-                          colClasses=c("character", # Composite Element REF
-                                       "numeric",   # beta value
-                                       "character", # Gene symbol
-                                       "character",   # Chromosome
-                                       "integer"))  # Genomic coordinate
-            setnames(data,gsub(" ", "\\.", colnames(data)))
-            setnames(data,2,barcode[i])
+            #data <- fread(files[i], header = TRUE, sep = "\t",
+            #              stringsAsFactors = FALSE,skip = 1,
+            #              colClasses=c("character", # Composite Element REF
+            #                           "numeric",   # beta value
+            #                           "character", # Gene symbol
+            #                           "character",   # Chromosome
+            #                           "integer"))  # Genomic coordinate
+            #setnames(data,gsub(" ", "\\.", colnames(data)))
+            #setnames(data,2,barcode[i])
+
+            data <- read.table(files[i],
+                               header = T,
+                               sep="\t",
+                               skip = 1,
+                               stringsAsFactors = FALSE)
+
+           colnames(data) <- gsub(" ", "\\.", colnames(data))
+           colnames(data)[2] <- barcode[i]
 
             if (i == 1) {
-                setcolorder(data,c(1, 3:5, 2))
+                #setcolorder(data,c(1, 3:5, 2))
+                data <- data[,c(1, 3:5, 2)]
                 df <- data
             } else {
                 data <- subset(data,select = c(1,2))
