19b9551b9b8dde1aec9ca7b4659acc12a21d0018
torongs82 <antonio.colaprico@gmail.com>
2015-09-01 23:22:10 +0200

    Moving functions in sections TCGAanalyze_DEA_Affy, TCGAprepare_Affy, TCGAquery_Investigate, TCGAquery_Social, TCGAquery_integrate, TCGAvisualize_Heatmap, TCGAvisualize_Tables
    Former-commit-id: 0750e12bcd669bf053ae8393330176ebf42ba4e9 [formerly 4ee72ac4734e91581ef0ea4cdb823c03e59b3a47]
    Former-commit-id: 2773763b61b885af87ad93e1600d26a6853a06ab

--- R/TCGAPrepare.R
+++ R/TCGAPrepare.R
@@ -948,3 +948,28 @@ getMage <- function(line){
     return(df)
 }
 
+#' @title Prepare CEL files into an AffyBatch.
+#' @description Prepare CEL files into an AffyBatch.
+#' @param ClinData write
+#' @param PathFolder write
+#' @param TabCel write
+#' @importFrom affy ReadAffy
+#' @importFrom affy rma
+#' @importFrom Biobase exprs
+#' @examples
+#' \dontrun{
+#' to add example
+#' }
+#' @export
+#' @return Normalizd Expression data from Affy eSets
+TCGAprepare_Affy <- function(ClinData, PathFolder, TabCel){
+
+    affy_batch <- ReadAffy(filenames=as.character(paste(TabCel$samples, ".CEL", sep="")))
+
+    eset <- rma(affy_batch)
+
+    mat <- exprs(eset)
+
+    return(mat)
+
+}
