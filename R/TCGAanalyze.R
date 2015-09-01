ff76fb59b373d2d42bd2b82f07b4605abbf5e93e
torongs82 <antonio.colaprico@gmail.com>
2015-09-01 23:22:10 +0200

    Moving functions in sections TCGAanalyze_DEA_Affy, TCGAprepare_Affy, TCGAquery_Investigate, TCGAquery_Social, TCGAquery_integrate, TCGAvisualize_Heatmap, TCGAvisualize_Tables

--- R/TCGAanalyze.R
+++ R/TCGAanalyze.R
@@ -873,3 +873,85 @@ TCGAanalyze_EA <- function(GeneName,RegulonList,TableEnrichment,
     return(topPathways_tab)
 }
 
+#' @title Differentially expression analysis (DEA) using limma package.
+#' @description Differentially expression analysis (DEA) using limma package.
+#' @param Pdatatable write
+#' @param PathFolder write
+#' @param FC.cut write
+#' @param AffySet A matrix-like data object containing log-ratios or log-expression values
+#' for a series of arrays, with rows corresponding to genes and columns to samples
+#' @importFrom limma lmFit
+#' @importFrom limma eBayes
+#' @importFrom limma makeContrasts
+#' @importFrom limma contrasts.fit
+#' @importFrom limma toptable
+#' @examples
+#' \dontrun{
+#' to add example
+#' }
+#' @export
+#' @return List of list with tables in 2 by 2 comparison
+#' of the top-ranked genes from a linear model fitted by DEA's limma
+#' @examples
+#' query <- TCGAquery(tumor = "lgg")
+TCGAanalyze_DEA_Affy <- function(Pdatatable, AffySet, PathFolder, FC.cut = 0.01){
+
+    f <- factor(Pdatatable$Disease)
+    groupColors<-names(table(f))
+
+    tmp<-matrix(0,length(groupColors),length(groupColors))
+    colnames(tmp) <- groupColors
+    rownames(tmp) <- groupColors
+    tmp[upper.tri(tmp)] <- 1
+
+
+    sample_tab<-Pdatatable
+    f <- factor(Pdatatable$Disease)
+    design <- model.matrix(~0+f)
+    colnames(design) <- levels(f)
+    fit <- lmFit(AffySet, design) ## fit is an object of class MArrayLM.
+
+    groupColors <- names(table(Pdatatable$Disease))
+
+    CompleteList<-vector("list",sum(tmp))
+
+    k<-1
+
+    for( i in 1: length(groupColors)){
+        col1 <- colnames(tmp)[i]
+        for( j in 1: length(groupColors)){
+            col2 <- rownames(tmp)[j]
+
+            if( i!=j ){
+
+                if(tmp[i,j]!=0){
+
+
+                    Comparison <- paste(col2,"-",col1,sep="")
+
+                    if(i==4 && j==6){ Comparison <- paste(col1,"-",col2,sep="") }
+                    if(i==5 && j==6){ Comparison <- paste(col1,"-",col2,sep="") }
+
+                    print( paste(i, j, Comparison,"to do..." ))
+
+                    cont.matrix <- makeContrasts(I=Comparison,levels=design)
+
+                    fit2 <- contrasts.fit(fit, cont.matrix)
+                    fit2 <- eBayes(fit2)
+
+
+
+                    sigI <- limma::topTable(fit2,coef=1, adjust.method="BH", sort.by="B", p.value = 0.05, lfc = FC.cut, number = 50000)
+
+                    sigIbis <- sigI[order(abs(as.numeric(sigI$logFC)), decreasing=TRUE),]
+                    names(CompleteList)[k]<-gsub("-","_",Comparison)
+                    CompleteList[[k]]<-sigIbis
+                    k<-k+1
+                }
+            }
+        }
+
+    }
+
+    return(CompleteList)
+}
