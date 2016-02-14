1648901fb8dad364bfd62c12071109fde788af87
Tiago Chedraoui Silva <tiagochst@usp.br>
2016-02-14 02:42:05 +0000

    Merge branch 'master' into devel
    * master:
      Updating mysjplot
      Updating NEWS
      Correction on the number of folders
      Checking integrity of the file
      Starting download verification, needs more test
    
    From: tiago <tiagochst@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TCGAbiolinks@113612 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TCGAvisualize.R
+++ R/TCGAvisualize.R
@@ -1189,13 +1189,16 @@ TCGAvisualize_mutation <- function (data = NULL,
     # --------------------------------------------------------
     # try to automatically set labels is not passed as parameter
     # --------------------------------------------------------
-    if (is.null(legendLabels)) legendLabels <- sjmisc:::autoSetValueLabels(items[[1]])
+    if (is.null(legendLabels)) legendLabels <- sjmisc::get_labels(items[[1]],
+                                                                  attr.only = F,
+                                                                  include.values = NULL,
+                                                                  include.non.labelled = T)
     if (is.null(axisLabels.y)) {
         axisLabels.y <- c()
         # if yes, iterate each variable
         for (i in 1:ncol(items)) {
             # retrieve variable name attribute
-            vn <- sjmisc:::autoSetVariableLabels(items[[i]])
+            vn <- sjmisc::get_label(items[[i]], def.value = colnames(items)[i])
             # if variable has attribute, add to variableLabel list
             if (!is.null(vn)) {
                 axisLabels.y <- c(axisLabels.y, vn)
