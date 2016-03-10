f15baa0e85d0ce6aa12ff37befa2ef8ea436aef5
Tiago Chedraoui Silva <tiagochst@usp.br>
2016-03-10 12:23:48 +0000

    Merge branch 'master' into devel
    * master: (766 commits)
      Db update
      This closes #4. EAGenes are inside the package.
      Fixing mean methylation plot. If only one group.
      Return plot if filename is set to null
      db update
      Improving miRNA output from TCGAprepare
      version bump
      db update
      Bug fix
      Improving doc
      Version bump
      Version bump
      updating subtypes
      updating subtypes info
      Add option to add/remove tsubtypes from TCGAprepare
      Adding new subtypes: pra kirp kirc,
      Addin THCA
      Adding new subtypes
      Removing warnings
      Solving windows bug, data has to be downloaded as binary
      ...
    
    From: tiagochst <tiagochst@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TCGAbiolinks@114603 bc3139a8-67e5-0310-9ffc-ced21a209358
    
    Former-commit-id: c8ae680b8cc833701058b078956eac256900ccf9 [formerly eccd5ebc88712bf5ab628c02233ae0e7b4c82c2a]
    Former-commit-id: 32b45c2d0879509cf257f257ceffa1b6e0affe43

--- R/methylation.R
+++ R/methylation.R
@@ -304,17 +304,19 @@ TCGAvisualize_meanMethylation <- function(data,
     #comb2by2 <- combinations(length(levels(droplevels(df$groups))),
     #                  2,
     #                 levels(droplevels(df$groups)))
-    comb2by2 <- t(combn(levels(droplevels(df$groups)),2))
+    groups <- levels(droplevels(df$groups))
+    if(length(groups) > 1){
+        comb2by2 <- t(combn(levels(droplevels(df$groups)),2))
 
-    for (i in 1:nrow(comb2by2)){
-        try({
-            aux <- t.test(mean ~ groups,
-                          data = subset(df,subset=df$groups %in% comb2by2[i,]) )$p.value;
-            message(paste("P-value:", paste0(comb2by2[i,], collapse = "-"),"=",aux))},
-            silent = TRUE
-        )
+        for (i in 1:nrow(comb2by2)){
+            try({
+                aux <- t.test(mean ~ groups,
+                              data = subset(df,subset=df$groups %in% comb2by2[i,]) )$p.value;
+                message(paste("P-value:", paste0(comb2by2[i,], collapse = "-"),"=",aux))},
+                silent = TRUE
+            )
+        }
     }
-
     if(print.pvalue & length(levels(droplevels(df$groups))) == 2) {
         pvalue <- t.test(mean ~ groups, data = df)$p.value
     } else {
@@ -389,8 +391,12 @@ TCGAvisualize_meanMethylation <- function(data,
                                                 digits = 2)))
     }
     # saving box plot to analyse it
-    ggsave(p, filename = filename, width = 10, height = 10, dpi = 600)
-    message(paste("Plot saved in: ", file.path(getwd(),filename)))
+    if(!is.null(filename)){
+        ggsave(p, filename = filename, width = 10, height = 10, dpi = 600)
+        message(paste("Plot saved in: ", file.path(getwd(),filename)))
+    } else {
+        return(p)
+    }
 }
 
 #' @title Calculate pvalues
