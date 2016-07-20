6657909e3b5f8fceb4df190609d1d862bf406536
tiagochst <tiagochst@gmail.com>
2016-07-19 21:10:03 -0300

    small bug fix
    Former-commit-id: d9094a5e6ac5a3fe277f35d9798c3a944912e5f0 [formerly 74469fecbbe7d068dc501f077278f1ca1fa1e34c]
    Former-commit-id: 18310cdb5e3ea16f267c058b4a487095b62a5107

--- R/TCGAQuery.R
+++ R/TCGAQuery.R
@@ -178,7 +178,7 @@ expandBarcodeInfo <- function(barcode){
 
 
 getBarcodeDefinition <- function(type = "TCGA"){
-    if(type = "TCGA"){
+    if(type == "TCGA"){
         tissue.code <- c('01','02','03','04','05','06','07','08','09','10','11',
                          '12','13','14','20','40','50','60','61')
         shortLetterCode <- c("TP","TR","TB","TRBM","TAP","TM","TAM","THOC",
