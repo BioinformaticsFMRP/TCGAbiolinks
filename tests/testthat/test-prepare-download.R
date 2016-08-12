36d3d7635d7b16dc031f01b3e7ce5c9bcd78f5d5
Tiago Chedraoui Silva <tiagochst@usp.br>
2016-08-12 19:19:20 +0000

    fixing to new version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TCGAbiolinks@120100 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-prepare-download.R
+++ tests/testthat/test-prepare-download.R
@@ -15,6 +15,20 @@ test_that("GDCdownload works", {
                         query$results[[1]]$file_name)
      expect_true(all(file.exists(files)))
 
+     query <- GDCquery(project = "TCGA-ACC",
+                       data.category =  "Copy number variation",
+                       legacy = TRUE,
+                       file.type = "hg19.seg",
+                       barcode = c("TCGA-OR-A5LR-01A-11D-A29H-01"))
+     # data will be saved in  GDCdata/TCGA-ACC/legacy/Copy_number_variation/Copy_number_segmentation
+     GDCdownload(query, method = "api", directory = "example_data_dir")
+     files <- file.path("example_data_dir/TCGA-ACC/legacy/",
+                        gsub(" ","_",query$results[[1]]$data_category),
+                        gsub(" ","_",query$results[[1]]$data_type),
+                        query$results[[1]]$file_id,
+                        query$results[[1]]$file_name)
+     expect_true(all(file.exists(files)))
+
      query <- GDCquery(project = "TARGET-AML",
                        data.category = "Transcriptome Profiling",
                        data.type = "miRNA Expression Quantification",
@@ -34,4 +48,14 @@ test_that("GDCdownload works", {
     unlink("example_data_dir",recursive = TRUE, force = TRUE)
 })
 
+test_that("getBarcodeInfo works", {
+    cols <- c("gender","project_id","days_to_last_follow_up","alcohol_history","cigarettes_per_day")
+    x <- getBarcodeInfo(c("TCGA-OR-A5LR", "TCGA-OR-A5LJ"))
+    expect_true(all(cols %in% colnames(x)))
+
+    cols <- c("gender","project_id")
+    x <- getBarcodeInfo(c("TARGET-20-PARUDL"))
+    expect_true(all(cols %in% colnames(x)))
+
+})
 
