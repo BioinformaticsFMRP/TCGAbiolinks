d92febc31681f535906d2e7fe8cfb7c848226573
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2016-09-26 13:59:32 -0700

    Connection improving

--- R/TCGAquery_clinical.R
+++ R/TCGAquery_clinical.R
@@ -306,7 +306,7 @@ GDCquery_clinic <- function(project, type = "clinical", save.csv = FALSE){
                              URLencode('"]}},{"op":"in","content":{"field":"files.data_category","value":["'),
                              files.data_category,
                              URLencode('"]}}]}'))
-    url <- paste0(baseURL,paste(options.pretty,options.expand, option.size, options.filter, sep = "&"))
+    url <- paste0(baseURL,paste(options.pretty,options.expand, option.size, options.filter,"format=json", sep = "&"))
     json  <- tryCatch({
         json <- fromJSON(url, simplifyDataFrame = TRUE)
     }, error = function(e) {
