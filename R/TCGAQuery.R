4be181d0252ac283c9c32c5a1f7638c95e32d5b0
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2016-11-04 11:41:51 -0700

    Keeping names with 2 varscan2 and mutect2, but mutect and varcan as input will also be considered Fixes #42

--- R/TCGAQuery.R
+++ R/TCGAQuery.R
@@ -386,7 +386,7 @@ getBarcodeDefinition <- function(type = "TCGA"){
 #'   GDCquery_Maf uses the following guide to download maf files
 #'   https://gdc-docs.nci.nih.gov/Data/Release_Notes/Data_Release_Notes/
 #' @param pipelines Four separate variant calling pipelines are implemented for GDC data harmonization.
-#' Options: muse, varscan2, somaticsniper, MuTect2. For more information:
+#' Options: muse, varscan2, somaticsniper, mutect2. For more information:
 #' https://gdc-docs.nci.nih.gov/Data/Bioinformatics_Pipelines/DNA_Seq_Variant_Calling_Pipeline/
 #' @param tumor a valid tumor
 #' @param save.csv Write maf file into a csv document
@@ -402,19 +402,19 @@ getBarcodeDefinition <- function(type = "TCGA"){
 #' \dontrun{
 #'   acc.varscan2.maf <- GDCquery_Maf("ACC", pipelines = "varscan2")
 #'    acc.somaticsniper.maf <- GDCquery_Maf("ACC", pipelines = "somaticsniper")
-#'    acc.mutect.maf <- GDCquery_Maf("ACC", pipelines = "mutect")
+#'    acc.mutect.maf <- GDCquery_Maf("ACC", pipelines = "mutect2")
 #' }
 #' @return A data frame with the maf file information
 GDCquery_Maf <- function(tumor, save.csv= FALSE, directory = "GDCdata", pipelines = NULL){
 
-    if(is.null(pipelines)) stop("Please select the pipeline argument (muse, varscan2, somaticsniper, mutect)")
-    if(pipelines == "varscan2") {
+    if(is.null(pipelines)) stop("Please select the pipeline argument (muse, varscan2, somaticsniper, mutect2)")
+    if(grepl("varscan",pipelines, ignore.case = TRUE)) {
         workflow.type <- "VarScan2 Variant Aggregation and Masking"
     } else if(pipelines == "muse") {
         workflow.type <- "MuSE Variant Aggregation and Masking"
     } else if(pipelines == "somaticsniper") {
         workflow.type <- "SomaticSniper Variant Aggregation and Masking"
-    } else if(pipelines == "mutect") {
+    } else if(grepl("mutect",pipelines, ignore.case = TRUE)) {
         workflow.type <-  "MuTect2 Variant Aggregation and Masking"
     } else {
         stop("Please select the pipeline argument (muse, varscan2, somaticsniper, mutect2)")
