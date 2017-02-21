fe8427e55d873c7548e062c32dc88698180123cf
tiagochst <tiagochst@gmail.com>
2017-02-20 22:51:19 -0300

    Bug fix in plot if there are NAs
    Former-commit-id: 640fc8ef8934a98fbf4e39db547ee6abab8e43fc

--- R/TCGAvisualize.R
+++ R/TCGAvisualize.R
@@ -774,7 +774,7 @@ TCGAvisualize_Heatmap <- function(data,
     }
 
     if(is.null(extrems)) {
-        if(min(data) < 0) {
+        if(min(data,na.rm = T) < 0) {
             extrems <- c(min(data), (max(data) + min(data))/2, max(data))
         } else {
             extrems <- c(0, max(data)/2, max(data))
