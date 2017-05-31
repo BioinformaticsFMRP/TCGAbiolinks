07bacd54f2c37e7c1340382d5e88da2f710996dd
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2017-05-31 14:41:50 -0700

    Adding message for error in trycatch

--- R/query.R
+++ R/query.R
@@ -154,6 +154,8 @@ GDCquery <- function(project,
         json  <- tryCatch(
             getURL(url,fromJSON,timeout(600),simplifyDataFrame = TRUE),
             error = function(e) {
+                message(paste("Error: ", e, sep = " "))
+                message("We will retry to access GDC!")
                 fromJSON(content(getURL(url,GET,timeout(600)), as = "text", encoding = "UTF-8"), simplifyDataFrame = TRUE)
             }
         )
