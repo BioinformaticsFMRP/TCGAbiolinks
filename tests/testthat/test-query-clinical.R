0e6f1bfd18450c3e2a09f6c81383f9607be66904
Tiago Chedraoui Silva <tiagochst@gmail.com>
2019-12-05 10:46:59 -0500

    Improve clinical for non TCGA projects
    Former-commit-id: 8b2a2bc6ebf7f10801545d93bde61f9ef3f0578e [formerly ae318219d5184b6be4f5dbf9a7d975138e2fbefb]
    Former-commit-id: 52b9864a3718e7a3f746ccde7cafa5114eafbc5c

--- tests/testthat/test-query-clinical.R
+++ tests/testthat/test-query-clinical.R
@@ -8,6 +8,18 @@ test_that("TCGAquery_SampleTypes returns the correct barcodes", {
     expect_equal(TCGAquery_SampleTypes(barcode,c("TN")),"Error message: one or more sample types do not exist")
 })
 
+
+test_that("GDCquery_clinic  populates correctly the data", {
+    results <- GDCquery_clinic( "BEATAML1.0-COHORT")
+    results.2028 <- results[results$submitter_id == "2028",]
+    expect_equal(results.2028$vital_status,"Alive")
+    expect_true(all(c("BA2486D","BA2144D") %in%
+                        (str_split(results.2028$submitter_sample_ids,",") %>% unlist())))
+    expect_equal(results.2028$age_at_diagnosis %% 365.25,134)
+    expect_equal(as.integer(results.2028$age_at_diagnosis / 365.25),56)
+})
+
+
 test_that("TCGAquery_MatchedCoupledSampleTypes returns the correct barcodes", {
     barcode <-c("TCGA-B0-4698-01Z-00-DX1",
                 "TCGA-B0-4698-02Z-00-DX1",
