8d0502ac2b65d7d9426e724cc4aa441d91301e4c
Tiago <tiagochst@gmail.com>
2020-07-15 09:39:22 -0400

    Skip on bioc
    Former-commit-id: 7e48cafc5c2e4e4ab69f7b3171f1b8d6e09f9b1e

--- tests/testthat/test-prepare-download.R
+++ tests/testthat/test-prepare-download.R
@@ -1,14 +1,18 @@
 context("Download AND PREPARE")
 
 test_that("GDCdownload API method is working ", {
+    skip_on_bioc()
+    skip_if_offline()
+
     cases <-  c("TCGA-OR-A5JX-01A-11R-A29S-07",
                 "TCGA-OR-A5KY-01A-11R-A29S-07",
                 "TCGA-PK-A5HA-01A-11R-A29S-07")
-    acc.gbm <- GDCquery(project =  c("TCGA-ACC"),
-                        data.category = "Transcriptome Profiling",
-                        data.type = "Gene Expression Quantification",
-                        workflow.type = "HTSeq - FPKM-UQ",
-                        barcode = substr(cases,1,12))
+    acc.gbm <- GDCquery(
+        project =  c("TCGA-ACC"),
+        data.category = "Transcriptome Profiling",
+        data.type = "Gene Expression Quantification",
+        workflow.type = "HTSeq - FPKM-UQ",
+        barcode = substr(cases,1,12))
     GDCdownload(acc.gbm, method = "api", directory = "ex")
     obj <- GDCprepare(acc.gbm,  directory = "ex",summarizedExperiment = FALSE)
     expect_true(all(substr(colnames(obj)[-1],1,12) == substr(cases,1,12)))
@@ -34,6 +38,9 @@ test_that("GDCdownload API method is working ", {
 
 
 test_that("getBarcodeInfo works", {
+    skip_on_bioc()
+    skip_if_offline()
+
     cols <- c("gender","project_id","days_to_last_follow_up","alcohol_history")
     x <- getBarcodeInfo(c("TCGA-OR-A5LR-01A", "TCGA-OR-A5LJ-01A"))
     expect_true(all(cols %in% colnames(x)))
@@ -66,11 +73,11 @@ test_that("getBarcodeInfo works", {
     expect_true(x[x$sample_submitter_id == "HCM-CSHL-0065-C20-06A","sample_type"] == "Metastatic")
     expect_true(x[x$sample_submitter_id == "HCM-CSHL-0063-C18-85A","sample_type"] == "Next Generation Cancer Model")
 
-
-
 })
 
 test_that("colDataPrepare handle replicates", {
+    skip_on_bioc()
+    skip_if_offline()
     barcodes <- c("TCGA-06-0156-01A-02R-1849-01","TCGA-06-0156-01A-03R-1849-01")
     x <- colDataPrepare(barcodes)
     expect_true(nrow(x) == 2)
@@ -79,6 +86,8 @@ test_that("colDataPrepare handle replicates", {
 })
 
 test_that("GDCprepare accepts more than one project", {
+    skip_on_bioc()
+    skip_if_offline()
     cases <-  c("TCGA-OR-A5JX-01A", "TCGA-OR-A5J3-01A",
                 "TCGA-06-0680-11A","TCGA-14-0871-01A")
     expect_true(all(c("TCGA-ACC","TCGA-GBM") %in% colDataPrepare(cases)$project_id))
@@ -93,25 +102,30 @@ test_that("GDCprepare accepts more than one project", {
 })
 
 test_that("Non TCGA data is processed", {
-     proj <- "MMRF-COMMPASS"
-     query <- GDCquery(
-         project = proj,
+    skip_on_bioc()
+    skip_if_offline()
+    proj <- "MMRF-COMMPASS"
+    query <- GDCquery(
+        project = proj,
         data.category = "Transcriptome Profiling",
         data.type = "Gene Expression Quantification",
         workflow.type = "STAR - Counts"
-     )
-     query <- GDCquery(
-         project = proj,
-         data.category = "Transcriptome Profiling",
-         data.type = "Gene Expression Quantification",
-         workflow.type = "STAR - Counts",
-         barcode = getResults(query)$cases[1:4]
-     )
-     GDCdownload(query)
-     data <- GDCprepare(query)
+    )
+    query <- GDCquery(
+        project = proj,
+        data.category = "Transcriptome Profiling",
+        data.type = "Gene Expression Quantification",
+        workflow.type = "STAR - Counts",
+        barcode = getResults(query)$cases[1:4]
+    )
+    GDCdownload(query)
+    data <- GDCprepare(query)
 })
 
 test_that("GISTIC2 data is being correclty prepare", {
+    skip_on_bioc()
+    skip_if_offline()
+
     query <- GDCquery(project = "TCGA-COAD",
                       data.category = "Copy Number Variation",
                       data.type = "Gene Level Copy Number Scores",
@@ -129,23 +143,29 @@ test_that("GISTIC2 data is being correclty prepare", {
 })
 
 test_that("IDAT files is processed", {
- #   proj <- "TCGA-LUAD"
- #   query <- GDCquery(project = proj,
- #                     data.category = "Raw microarray data",
- #                     data.type = "Raw intensities",
- #                     experimental.strategy = "Methylation array",
- #                     legacy = TRUE,
- #                     file.type = ".idat",
- #                     barcode = "TCGA-55-7724",
- #                     platform = "Illumina Human Methylation 450")
- #    tryCatch(GDCdownload(query, method = "api", files.per.chunk = 20),
- #             error = function(e) GDCdownload(query, method = "client"))
- #    betas <- GDCprepare(query)
- #    expect_true(nrow(betas) == 485577)
- #    expect_true(ncol(betas) == 1)
+    skip_on_bioc()
+    skip_if_offline()
+
+    proj <- "TCGA-LUAD"
+    query <- GDCquery(project = proj,
+                      data.category = "Raw microarray data",
+                      data.type = "Raw intensities",
+                      experimental.strategy = "Methylation array",
+                      legacy = TRUE,
+                      file.type = ".idat",
+                      barcode = "TCGA-55-7724",
+                      platform = "Illumina Human Methylation 450")
+    tryCatch(GDCdownload(query, method = "api", files.per.chunk = 20),
+             error = function(e) GDCdownload(query, method = "client"))
+    betas <- GDCprepare(query)
+    expect_true(nrow(betas) == 485577)
+    expect_true(ncol(betas) == 1)
 })
 
 test_that("Prepare samples without clinical data", {
+    skip_on_bioc()
+    skip_if_offline()
+
     # x <-  GDCquery_clinic(project = "TCGA-LUAD", type = "clinical")
     # x[is.na(x$diagnosis_id),]
     x <- colDataPrepare(c("TCGA-80-5608-01A","TCGA-17-Z053-01A","TCGA-78-7158-01A"))
@@ -153,6 +173,9 @@ test_that("Prepare samples without clinical data", {
 })
 
 test_that("Prepare multiple samples from the same patient", {
+    skip_on_bioc()
+    skip_if_offline()
+
     # https://portal.gdc.cancer.gov/cases/d7d3de82-802d-4664-8e42-d40408b129b0?bioId=548a300f-a7eb-4dc0-b9bc-5a643ef03d5d
     x <- colDataPrepare(c("BA2691R","BA2577R","BA2748R","BA2577D"))
     expect_true(nrow(x) == 4)
