5a148d34e48b70c257a324a31cead4df1f7e03b9
Tiago Silva <tiagosilva@Tiagos-MacBook-Pro-2.local>
2022-09-19 14:19:21 -0400

    Updating tests age at diagnosis is a character due to NAs

--- tests/testthat/test-query-clinical.R
+++ tests/testthat/test-query-clinical.R
@@ -15,14 +15,14 @@ test_that("GDCquery_clinic populates correctly the data", {
     expect_equal(results.2028$vital_status,"Alive")
     expect_true(all(c("BA2486D","BA2144D") %in%
                         (str_split(results.2028$submitter_sample_ids,",") %>% unlist())))
-    expect_equal(results.2028$age_at_diagnosis %% 365.25,134)
-    expect_equal(as.integer(results.2028$age_at_diagnosis / 365.25),56)
+    expect_equal(results.2028$age_at_diagnosis %>% as.numeric() %% 365.25,134)
+    expect_equal(as.integer(results.2028$age_at_diagnosis %>% as.numeric() / 365.25),56)
 
     results <- GDCquery_clinic(project = "ORGANOID-PANCREATIC", type = "clinical")
     results.42 <- results[results$submitter_id == "42",]
     expect_equal(results.42$ajcc_pathologic_stage,"Stage IIA")
     expect_equal(results.42$ethnicity,"not hispanic or latino")
-    expect_equal(as.integer(results.2028$age_at_diagnosis / 365.25),56)
+    expect_equal(as.integer(results.2028$age_at_diagnosis %>% as.numeric() / 365.25),56)
 
     results <- GDCquery_clinic( "TCGA-LUAD")
     results.sample <- results[results$submitter_id == "TCGA-80-5608",]
@@ -39,11 +39,13 @@ test_that("GDCquery_clinic populates correctly the data", {
 
 
 test_that("TCGAquery_MatchedCoupledSampleTypes returns the correct barcodes", {
-    barcode <- c("TCGA-B0-4698-01Z-00-DX1",
-                 "TCGA-B0-4698-02Z-00-DX1",
-                 "TCGA-CD-4698-02Z-00-DX1",
-                 "TCGA-XO-4698-01Z-00-DX1",
-                 "TCGA-XO-4698-11Z-00-DX1")
+    barcode <- c(
+        "TCGA-B0-4698-01Z-00-DX1",
+        "TCGA-B0-4698-02Z-00-DX1",
+        "TCGA-CD-4698-02Z-00-DX1",
+        "TCGA-XO-4698-01Z-00-DX1",
+        "TCGA-XO-4698-11Z-00-DX1"
+    )
 
     expect_equal(TCGAquery_MatchedCoupledSampleTypes(barcode, c("TP","TR")),barcode[1:2])
     expect_equal(TCGAquery_MatchedCoupledSampleTypes(barcode, c("TP","NT")),barcode[4:5])
